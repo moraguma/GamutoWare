1ce85d5ca359424526fc14feb7c45b8803e8a431
Gustavo Araújo Morais <g170603@dac.unicamp.br>
2022-06-03 13:06:16 -0300

    Adds whale microgame

---
+++ microjogos/2022S1/baleia/cenas/baleia.gd
@@ -0,0 +1,50 @@
+extends KinematicBody2D
+ 
+const TIME_PERIOD = 0.8
+var time = 0
+onready var AguaSound = get_node("AguaSound")
+onready var BaleiaHit = get_node("baleiaHit")
+onready var background = get_node("background")
+onready var baleia_animacao = get_node("baleia_animacao")
+onready var baleia_morta = get_node("baleia_morta")
+onready var base = get_parent()
+var vivo = true
+
+func _ready():
+	match Global.language:
+		Global.LANGUAGE.PT:
+			NotificationCenter.notify("MATE A BALEIA")
+		Global.LANGUAGE.EN:
+			NotificationCenter.notify("KILL THE WHALE")
+	background.play()
+			
+	
+func _on_Terra_body_entered(body):
+	BaleiaHit.play()
+	baleia_animacao.visible = false
+	baleia_morta.visible = true
+	vivo = false
+	base.emit_signal("win")
+
+func _on_agua_body_entered(body):
+	AguaSound.play()
+	hide()
+	base.emit_signal("lose")
+	
+
+func _physics_process(delta):
+	time += delta
+	if vivo && time > TIME_PERIOD:
+		var velocidade = Vector2(0,10)
+		if Input.is_action_pressed("direita"):
+			velocidade.x = 500
+	
+		if Input.is_action_pressed("esquerda"):
+			velocidade.x = -500
+	
+		velocidade.y= 30000 * delta
+		velocidade = move_and_slide(velocidade)
+	
+
+
+
---
+++ microjogos/2022S1/baleia/cenas/baleiaGame.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+const WIDTH = 1920
+const HEIGHT = 1080
+
+
+signal win
+signal lose
