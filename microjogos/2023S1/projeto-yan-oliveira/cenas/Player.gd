1d43275d75401b0cc6ef929951ec0c6e530c8abb
Yan Oliveira <supremoyan@gmail.com>
2023-04-01 21:05:58 -0300

    Primeiro dia

---
+++ microjogos/2023S1/projeto-yan-oliveira/cenas/Player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+var velocidade = 0.08
+var giro = 0
+func _physics_process(delta):
+	rotate(giro)
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		giro = direction * velocidade
+	else:
+		giro = 0
