1594d5d41d73051e9844a8cfa065612b617f4c9c
Olimpiada Brasileira de Informatica <obi20@jon.lab.ic.unicamp.br>
2023-04-02 18:30:28 -0300

    jogo do prato

--- microjogos/2023S1/projeto-repasch/cenas/CharacterBody2D.gd
+++ microjogos/2023S1/projeto-repasch/cenas/CharacterBody2D.gd
@@ -1,18 +1,21 @@
 extends CharacterBody2D
 
-
+var comi = 0
 const SPEED = 300.0
 
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-
 func _physics_process(delta):
-	# Add the gravity.
 	var dir = (Vector2(1,0)*(Input.get_action_strength("direita")-Input.get_action_strength("esquerda")))
-	#var direction = Input.get_axis("ui_left", "ui_right")
-	#if direction:
 	velocity = dir * SPEED
-	#else:
 	position += velocity*delta
-
 	move_and_slide()
+	
+func soma_comida():
+	comi += 1
+	if comi == 3:
+		get_parent().register_win()
+
+func _on_area_2d_area_entered(area):
+	#area.queue_free()
+	#soma_comida()
+	pass # Replace with function body.
