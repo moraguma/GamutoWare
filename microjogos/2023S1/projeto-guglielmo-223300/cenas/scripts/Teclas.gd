7efc1a7879478e32fa69880376c0603da6c64cf0
Guglielmo Gariglio <ra223300@smurfs.lab.ic.unicamp.br>
2023-04-02 19:21:10 -0300

    add typefixer game

--- microjogos/2023S1/projeto-guglielmo-223300/cenas/scripts/Teclas.gd
+++ microjogos/2023S1/projeto-guglielmo-223300/cenas/scripts/Teclas.gd
@@ -1,49 +1,89 @@
 extends Node2D 
 # Called when the node enters the scene tree for the first time.
-var r = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var r = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","Y","Z"]
 var i = 0
-var sel
-var lis_tec =[]
-var lis = []
+var j = 0
+var d1
+var d2
+var d3
+var d4
+var win
+@onready var sel_tec1
+@onready var sel_tec2
+@onready var sel_tec3
+@onready var sel_tec4
+@onready var lis = []
+@onready var lis1 = []
+@onready var lis_sel = []
 func _ready():
 	randomize()
-	sel = get_node("Teclas/Seletor")
+	sel_tec1 = get_node("Seletor teclado1")
+	sel_tec2 = get_node("Seletor teclado2")
+	sel_tec3 = get_node("Seletor teclado3")
+	sel_tec4 = get_node("Seletor teclado4")
+	lis_sel = [sel_tec1,sel_tec2,sel_tec3,sel_tec4]
 	var a = randi_range(0,r.size()-1)
-	a = r[a]
-	r.erase(a)
-	var T1 = get_node(a)
+	var A = r[a]
+	r.pop_at(a)
+	randomize()
 	var b = randi_range(0,r.size()-1)
-	b = r[b]
-	r.erase(b)
-	var T2 = get_node(b)
+	var B = r[b]
+	r.pop_at(b)
+	randomize()
 	var c = randi_range(0,r.size()-1)
-	c = r[c]
-	r.erase(c)
-	var T3 = get_node(c)
+	var C = r[c]
+	r.pop_at(c)
+	randomize()
 	var d = randi_range(0,r.size()-1)
-	d = r[d]
-	r.erase(d)
-	var T4 = get_node(d)
+	var D = r[d]
+	r.pop_at(d)
+	randomize()
+	var fil = [A,B,C,D]
+	fil.sort()
+	var T1 = get_node(fil[0])
+	var T2 = get_node(fil[1])
+	var T3 = get_node(fil[2])
+	var T4 = get_node(fil[3])
 	T1.visible = true
-	get_node("Teclas/A/RichTextLabel").text = a
+	get_node("Teclas/A/RichTextLabel").text = fil[0]
 	T2.visible = true
-	get_node("Teclas/A2/RichTextLabel").text = b
+	get_node("Teclas/A2/RichTextLabel").text = fil[1]
 	T3.visible = true
-	get_node("Teclas/A3/RichTextLabel").text = c
+	get_node("Teclas/A3/RichTextLabel").text = fil[2]
 	T4.visible = true
-	get_node("Teclas/A4/RichTextLabel").text = d
-	lis = [T1,T2,T3,T4]
+	get_node("Teclas/A4/RichTextLabel").text = fil[3]
+	d1 = T1.position
+	d2 = T2.position
+	d3 = T3.position
+	d4 = T4.position
+	var rnd = randi_range(0,3)
+	lis = [d1,d2,d3,d4]
+	lis1 = lis
+	sel_tec1.position = lis[rnd]
+	i = rnd
 	
 func _mudar():
-	pass
+	if(i!=0):
+		print('Aaaa')
+		win = false
+	lis.pop_at(i)
+	j+=1
+	if(j < 4):
+		var rnd = randi_range(0,lis.size()-1)
+		lis_sel[j].visible = true
+		lis_sel[j].position = lis[rnd]
+		i = rnd
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(Input.is_action_just_pressed("direita")):
 		var g = i+1
-		i = (g%4)
-		sel.transform.position = lis[i].transform.position
+		i = (g%lis.size())
+		lis_sel[j].position = lis[i]
 	if(Input.is_action_just_pressed("esquerda")):
-		print("DDDDD")
+		var g = i-1
+		i = (g%lis.size())
+		lis_sel[j].position = lis[i]
 	if(Input.is_action_just_pressed("acao")):
 		_mudar()
 		
--- microjogos/2023S1/projeto-guglielmo-223300/cenas/scripts/main.gd
+++ microjogos/2023S1/projeto-guglielmo-223300/cenas/scripts/main.gd
@@ -1,67 +1,102 @@
 extends Node2D
-
 # Declaração dos sinais win e lose
 signal win
 signal lose
-
-# Estas constantes são usadas para determinar o tamanho da tela do seu jogo. Por padrão, definem uma
-# tela 1920x1080, que é padrão para monitores full HD. Caso você queira uma resolução menor para 
-# atingir uma estética mais pixelada, você pode mudar estes números para qualquer outra resolução 
-# 16:9
 const WIDTH = 1920
 const HEIGHT = 1080
-
-
-# --------------------------------------------------------------------------------------------------
-# FUNÇÕES PADRÃO
-# --------------------------------------------------------------------------------------------------
-
-# Esta função é chamada assim que esta cena é instanciada, ou seja, assim que seu minigame inicia
+var r = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","Y","Z"]
+var i = 0
+var j = 0
+var d1
+var d2
+var d3
+var d4
+var A
+var a
+var B
+var b
+var C
+var c
+var D
+var d
+@onready var sel_tec1
+@onready var sel_tec2
+@onready var sel_tec3
+@onready var sel_tec4
+@onready var lis = []
+@onready var lis1 = []
+@onready var lis_sel = []
 func _ready():
-	# Verifica a linguagem do jogo e mostra texto nesta linguagem. Deve dar uma ideia do que deve
-	# ser feito para vencer o jogo. A fonte usada não suporta caracteres latinos como ~ ou ´
 	match Global.language:
 		Global.LANGUAGE.EN:
-			NotificationCenter.notify("DO SOMETHING!")
+			NotificationCenter.notify("FIX THE KEYBOARD")
 		Global.LANGUAGE.PT:
-			NotificationCenter.notify("FACA ALGO!")
+			NotificationCenter.notify("CONSERTE AS TECLAS!")
+	randomize()
+	sel_tec1 = get_node("Seletor teclado1")
+	sel_tec2 = get_node("Seletor teclado2")
+	sel_tec3 = get_node("Seletor teclado3")
+	sel_tec4 = get_node("Seletor teclado4")
+	lis_sel = [sel_tec1,sel_tec2,sel_tec3,sel_tec4]
+	while(a==b or a==c or a==d or b==c or b==d or c==d):
+		randomize()
+		a = randi_range(0,25)
+		A = r[a]
+		b = randi_range(0,r.size()-1)
+		B = r[b]
+		c = randi_range(0,r.size()-1)
+		C = r[c]
+		d = randi_range(0,r.size()-1)
+		D = r[d]
+	var fil = [A,B,C,D]
+	fil.sort()
+	var T1 = get_node(fil[0])
+	var T2 = get_node(fil[1])
+	var T3 = get_node(fil[2])
+	var T4 = get_node(fil[3])
+	T1.visible = true
+	get_node("Teclas/A/RichTextLabel").text = fil[0]
+	T2.visible = true
+	get_node("Teclas/A2/RichTextLabel").text = fil[1]
+	T3.visible = true
+	get_node("Teclas/A3/RichTextLabel").text = fil[2]
+	T4.visible = true
+	get_node("Teclas/A4/RichTextLabel").text = fil[3]
+	d1 = T1.position
+	d2 = T2.position
+	d3 = T3.position
+	d4 = T4.position
+	var rnd = randi_range(0,3)
+	lis = [d1,d2,d3,d4]
+	lis1 = lis
+	sel_tec1.position = lis[rnd]
+	i = rnd
+	
+func _mudar():
+	if(i!=0):
+		register_lose()
+	lis.pop_at(i)
+	j+=1
+	if(j < 4):
+		var rnd = randi_range(0,lis.size()-1)
+		lis_sel[j].visible = true
+		lis_sel[j].position = lis[rnd]
+		i = rnd
+	else:
+		register_win()
 
-
-# Esta função é chamada uma vez por frame e é otimizada para cálculos relacionados a física, como
-# a movimentação de um personagem. O parâmetro delta indica a quantidade de tempo que passou desde
-# a última chamada desta função. O comando pass não faz nada
-func _physics_process(delta):
-	pass
-
-
-# Esta função é chamada uma vez por frame e é otimizada para cálculos relacionados a renderização, 
-# como a movimentação de um personagem. O parâmetro delta indica a quantidade de tempo que passou 
-# desde a última chamada desta função. O comando pass não faz nada
 func _process(delta):
-	pass
+	if(Input.is_action_just_pressed("direita")):
+		var g = i+1
+		i = (g%lis.size())
+		lis_sel[j].position = lis[i]
+	if(Input.is_action_just_pressed("esquerda")):
+		var g = i-1
+		i = (g%lis.size())
+		lis_sel[j].position = lis[i]
+	if(Input.is_action_just_pressed("acao")):
+		_mudar()
 
-
-# --------------------------------------------------------------------------------------------------
-# SUAS FUNÇÕES
-# --------------------------------------------------------------------------------------------------
-
-
-# Um método genérico. Crie quantos métodos você precisar!
-func my_method():
-	pass
-
-
-# --------------------------------------------------------------------------------------------------
-# CONDIÇÕES DE VITÓRIA
-# --------------------------------------------------------------------------------------------------
-# Quando o jogo começa, ela assume que o jogador não conseguiu vencer o jogo ainda, ou seja, se não
-# acontecer nada, o jogador vai perder o jogo. A verificação se o jogador venceu o minigame é feita
-# com base na emissão dos sinais "win" e "lose". Se "win" foi o último sinal emitido, o jogador
-# vencerá o jogo, e se "lose" foi o último sinal emitido ou nenhum sinal foi emitido, o jogador
-# perderá o jogo
-
-
-# Chame esta função para registrar que o jogador venceu o jogo
 func register_win():
 	emit_signal("win")
 
