e42b37995a92f78f94a1a3b79a69fc3ea96b6d0e
Jose Victor Santana Barbosa <ra245511@pedrita.lab.ic.unicamp.br>
2023-04-02 19:38:18 -0300

    finalizacao do jogo

--- microjogos/2023S1/projeto-jose-victor/cenas/scripts/main.gd
+++ microjogos/2023S1/projeto-jose-victor/cenas/scripts/main.gd
@@ -1,71 +1,46 @@
 extends Node2D
 
-# Declaração dos sinais win e lose
 signal win
 signal lose
-
-# Estas constantes são usadas para determinar o tamanho da tela do seu jogo. Por padrão, definem uma
-# tela 1920x1080, que é padrão para monitores full HD. Caso você queira uma resolução menor para 
-# atingir uma estética mais pixelada, você pode mudar estes números para qualquer outra resolução 
-# 16:9
+ 
 const WIDTH = 1920
 const HEIGHT = 1080
+var pontuacao = 0
 
-
-# --------------------------------------------------------------------------------------------------
-# FUNÇÕES PADRÃO
-# --------------------------------------------------------------------------------------------------
-
-# Esta função é chamada assim que esta cena é instanciada, ou seja, assim que seu minigame inicia
-func _ready():
-	# Verifica a linguagem do jogo e mostra texto nesta linguagem. Deve dar uma ideia do que deve
-	# ser feito para vencer o jogo. A fonte usada não suporta caracteres latinos como ~ ou ´
+ 
+func _ready(): 
+	var random = RandomNumberGenerator.new()
+	random.randomize()
 	match Global.language:
 		Global.LANGUAGE.EN:
 			NotificationCenter.notify("DO SOMETHING!")
 		Global.LANGUAGE.PT:
 			NotificationCenter.notify("FACA ALGO!")
+	var spawns = [ 
+		{ "min": Vector2(96,896), "max": Vector2(1760,992) },
+		{ "min": Vector2(1760,208), "max": Vector2(1824,944) },
+		{ "min": Vector2(96,96), "max": Vector2(1760,160) },
+		{ "min": Vector2(96,208), "max": Vector2(160,944) }
+	]
+	var miojos = [$Miojin1, $Miojin2, $Miojin3, $Miojin4]
+	for i in range(4):
+		var spawn = spawns[i]  
+		miojos[i].position = Vector2(random.randi_range(spawn.min.x,spawn.max.x),random.randi_range(spawn.min.y,spawn.max.y))
+		
 
-
-# Esta função é chamada uma vez por frame e é otimizada para cálculos relacionados a física, como
-# a movimentação de um personagem. O parâmetro delta indica a quantidade de tempo que passou desde
-# a última chamada desta função. O comando pass não faz nada
-func _physics_process(delta):
-	pass
-
-
-# Esta função é chamada uma vez por frame e é otimizada para cálculos relacionados a renderização, 
-# como a movimentação de um personagem. O parâmetro delta indica a quantidade de tempo que passou 
-# desde a última chamada desta função. O comando pass não faz nada
 func _process(delta):
-	pass
+	print(pontuacao)
+
+ 
+func ganhar_ponto(body):
+	pontuacao += 1
+	if(pontuacao >= 4):
+		register_win()		
 
 
-# --------------------------------------------------------------------------------------------------
-# SUAS FUNÇÕES
-# --------------------------------------------------------------------------------------------------
-
-
-# Um método genérico. Crie quantos métodos você precisar!
-func my_method():
-	pass
-
-
-# --------------------------------------------------------------------------------------------------
-# CONDIÇÕES DE VITÓRIA
-# --------------------------------------------------------------------------------------------------
-# Quando o jogo começa, ela assume que o jogador não conseguiu vencer o jogo ainda, ou seja, se não
-# acontecer nada, o jogador vai perder o jogo. A verificação se o jogador venceu o minigame é feita
-# com base na emissão dos sinais "win" e "lose". Se "win" foi o último sinal emitido, o jogador
-# vencerá o jogo, e se "lose" foi o último sinal emitido ou nenhum sinal foi emitido, o jogador
-# perderá o jogo
-
-
-# Chame esta função para registrar que o jogador venceu o jogo
+ 
 func register_win():
-	emit_signal("win")
+	emit_signal("win") 
 
-
-# Chame esta função para registrar que o jogador perdeu o jogo
 func register_lose():
 	emit_signal("lose")
--- microjogos/2023S1/projeto-jose-victor/creditos.gd
+++ microjogos/2023S1/projeto-jose-victor/creditos.gd
@@ -1,23 +1,15 @@
 extends Resource
 
 
-# Este arquivo vai ser usado diretamente nos créditos do jogo! Dê crédito a todas as pessoaas que
-# participaram da criação do jogo e a todos os recursos externos utilizados. Lembre que a fonte que
-# será utilizada não suporte caracteres latinos como ~ ou ´
-
-
-# Título do seu jogo
-var title_pt = "Titulo em Portugues"
-var title_en = "Title in English"
+var title_pt = "Morcego"
+var title_en = "Bat"
 
 # Detalhes dos créditos do seu jogo
 var credits_pt = """
-Criado por: [Seu nome!]
-
-Você pode preencher o resto deste campo com creditos de assets utilizados, links para sua redes sociais ou qualquer outro detalhe que ache relevante
+Criado por: José Victor Santana Barbosa
+Texturas usadas do jogo Sneedy de Lazy Fox Licenciadas sobre  CC BY 4.0. 
 """
 var credits_en = """
-Made by: [Your name!]
-
-Fill this space with credits to used assets, links to your social medias and whatever other details you think are important
+Made by: José Victor Santana Barbosa
+Used textures from the game Sneedy by Lazy Fox Licensed under CC BY 4.0.
 """
