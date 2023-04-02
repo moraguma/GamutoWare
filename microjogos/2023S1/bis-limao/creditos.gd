b900e74a2f4834c88beedada544df90dd68f7c91
Rafael Campideli Hoyos <ra175100@ira.lab.ic.unicamp.br>
2023-04-02 19:08:02 -0300

    créditos

--- microjogos/2023S1/bis-limao/creditos.gd
+++ microjogos/2023S1/bis-limao/creditos.gd
@@ -7,17 +7,17 @@ extends Resource
 
 
 # Título do seu jogo
-var title_pt = "Titulo em Portugues"
-var title_en = "Title in English"
+var title_pt = "Bis Limao"
+var title_en = "Lemon Bis"
 
 # Detalhes dos créditos do seu jogo
 var credits_pt = """
-Criado por: [Seu nome!]
+Criado por: Rafael Hoyos
 
-Você pode preencher o resto deste campo com creditos de assets utilizados, links para sua redes sociais ou qualquer outro detalhe que ache relevante
+@rafachoyos
 """
 var credits_en = """
-Made by: [Your name!]
+Made by: Rafael Hoyos
 
-Fill this space with credits to used assets, links to your social medias and whatever other details you think are important
+@rafachoyos
 """
