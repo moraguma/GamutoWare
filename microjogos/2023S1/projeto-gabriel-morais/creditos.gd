caec999b94b61b105855cd6615f86ffce2a7221c
Gabriel Morais Barros <gabriel.morais.barros@gmail.com>
2023-04-03 13:05:13 -0300

    Adicionar assets nos creditos

--- microjogos/2023S1/projeto-gabriel-morais/creditos.gd
+++ microjogos/2023S1/projeto-gabriel-morais/creditos.gd
@@ -13,11 +13,17 @@ var title_en = "Newton's vengeance"
 # Detalhes dos créditos do seu jogo
 var credits_pt = """
 Criado por: Gabriel Morais Barros
-
-Você pode preencher o resto deste campo com creditos de assets utilizados, links para sua redes sociais ou qualquer outro detalhe que ache relevante
+A foto de Newton foi copiada de https://pt.wikipedia.org/wiki/Ficheiro:Sir_Isaac_Newton_(1643-1727).jpg, 
+que é uma pintura de Newton retratado por Godfrey Kneller, 1689 (com 46 anos de idade)
+As outras sprites foram feitas no site https://www.piskelapp.com/
+A música foi feita no site beepbox
+O barulho do estilingue foi retirado de freesound.org
 """
 var credits_en = """
 Made by: Gabriel Morais Barros
-
-Fill this space with credits to used assets, links to your social medias and whatever other details you think are important
+The picture of Isaac Newton came from the link https://pt.wikipedia.org/wiki/Ficheiro:Sir_Isaac_Newton_(1643-1727).jpg, 
+which is a representation of Newton by Godfrey Kneller, 1689 (with 46 years old)
+The other sprites were made on https://www.piskelapp.com/
+The music was made on the beepbox site
+The sound of the slingshot came from freesound.org
 """
