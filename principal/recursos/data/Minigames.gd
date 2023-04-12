c2a56ad2b141cc5ae1b05a4bd085d4f11e1c01f8
Moraguma <g170603@dac.unicamp.br>
2023-04-12 17:23:07 -0300

    Reverts Whale Eater implementation

--- principal/recursos/data/Minigames.gd
+++ principal/recursos/data/Minigames.gd
@@ -6,11 +6,11 @@ const minigame_packs = {
 	"Class of 2022": ["res://microjogos/2022S1/baleia/cenas/baleiaGame.tscn", "res://microjogos/2022S1/gamuturai/cenas/main.tscn", "res://microjogos/2022S1/race/cenas/main.tscn"],
 	"Demo": ["res://microjogos/demo/tiro_ao_alvo/cenas/main.tscn", "res://microjogos/demo/frogger/cenas/main.tscn"],
 	"Godotware": ["res://microjogos/godotware/Bang/Main.tscn", "res://microjogos/godotware/Simon/Main.tscn"],
-	"Moraguma": ["res://microjogos/moraguma/candle prick/cenas/Main.tscn", "res://microjogos/moraguma/deerskull/cenas/Main.tscn", "res://microjogos/moraguma/hora_da_necromancia/cenas/Main.tscn", "res://microjogos/moraguma/ifuv/cenas/Main.tscn", "res://microjogos/moraguma/whale_eater/cenas/Main.tscn"]
+	"Moraguma": ["res://microjogos/moraguma/candle prick/cenas/Main.tscn", "res://microjogos/moraguma/deerskull/cenas/Main.tscn", "res://microjogos/moraguma/hora_da_necromancia/cenas/Main.tscn", "res://microjogos/moraguma/ifuv/cenas/Main.tscn"]
 }
 
 # Removed from Moraguma
-# "res://microjogos/moraguma/soulchain/cenas/Main.tscn"
+# "res://microjogos/moraguma/soulchain/cenas/Main.tscn", "res://microjogos/moraguma/whale_eater/cenas/Main.tscn"
 
 const filters = {
 	"13+": ["res://microjogos/2022S1/baleia/cenas/baleiaGame.tscn"]
