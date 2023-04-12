a92a663a15b63f32978e7ca0b953f3e684806834
GoliasVictor <dev.jvsb@gmail.com>
2023-04-12 03:53:48 -0300

    Atualizando mingames.gd

--- principal/recursos/data/Minigames.gd
+++ principal/recursos/data/Minigames.gd
@@ -4,6 +4,7 @@ extends Resource
 const minigame_packs = {
 	"Demo": ["res://microjogos/demo/tiro_ao_alvo/cenas/main.tscn", "res://microjogos/demo/frogger/cenas/main.tscn"],
 	"Class of 2022": ["res://microjogos/2022S1/baleia/cenas/baleiaGame.tscn", "res://microjogos/2022S1/gamuturai/cenas/main.tscn", "res://microjogos/2022S1/race/cenas/main.tscn"],
+	"Class of 2023": ["res://microjogos/2023S1/projeto-jose-victor/cenas/main.tscn"],
 	"Godotware": ["res://microjogos/godotware/Bang/Main.tscn", "res://microjogos/godotware/Simon/Main.tscn"],
 	"Moraguma": ["res://microjogos/moraguma/candle prick/cenas/Main.tscn", "res://microjogos/moraguma/deerskull/cenas/Main.tscn", "res://microjogos/moraguma/hora_da_necromancia/cenas/Main.tscn"]
 }
@@ -22,6 +23,7 @@ const credit_paths = {
 	"res://microjogos/2022S1/baleia/cenas/baleiaGame.tscn": "res://microjogos/2022S1/baleia/creditos.gd",
 	"res://microjogos/2022S1/gamuturai/cenas/main.tscn": "res://microjogos/2022S1/gamuturai/creditos.gd",
 	"res://microjogos/2022S1/race/cenas/main.tscn": "res://microjogos/2022S1/race/creditos.gd",
+	"res://microjogos/2023S1/projeto-jose-victor/cenas/main.tscn": "res://microjogos/2023S1/projeto-jose-victor/creditos.gd",
 	"res://microjogos/godotware/Bang/Main.tscn": "res://microjogos/godotware/Bang/creditos.gd",
 	"res://microjogos/godotware/Simon/Main.tscn": "res://microjogos/godotware/Simon/creditos.gd",
 	"res://microjogos/moraguma/candle prick/cenas/Main.tscn": "res://microjogos/moraguma/candle prick/creditos.gd",
@@ -39,6 +41,7 @@ const cover_paths = {
 	"res://microjogos/2022S1/baleia/cenas/baleiaGame.tscn": "res://microjogos/2022S1/baleia/capa.png",
 	"res://microjogos/2022S1/gamuturai/cenas/main.tscn": "res://microjogos/2022S1/gamuturai/capa.png",
 	"res://microjogos/2022S1/race/cenas/main.tscn": "res://microjogos/2022S1/race/capa.png",
+	"res://microjogos/2023S1/projeto-jose-victor/cenas/main.tscn": "res://microjogos/2023S1/projeto-jose-victor/capa.png",
 	"res://microjogos/godotware/Bang/Main.tscn": "res://microjogos/godotware/Bang/capa.png",
 	"res://microjogos/godotware/Simon/Main.tscn": "res://microjogos/godotware/Simon/capa.png",
 	"res://microjogos/moraguma/candle prick/cenas/Main.tscn": "res://microjogos/moraguma/candle prick/capa.png",
