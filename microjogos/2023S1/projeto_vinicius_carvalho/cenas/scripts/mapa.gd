6bd4c0f2f849c9314b75a5d6d782be4805d09efd
Packss <enzoana@hotmail.com>
2023-04-25 11:56:54 -0300

    Bugs arrumados e recursos cortados, capa corrigida

---
+++ microjogos/2023S1/projeto_vinicius_carvalho/cenas/scripts/mapa.gd
@@ -0,0 +1,67 @@
+extends Node2D
+
+const WIDTH = 1920
+const HEIGHT = 1080
+
+var timer
+var timer_win
+var som
+var obstaculo1 = preload("res://microjogos/2023S1/projeto_vinicius_carvalho/cenas/obstaculo_1.tscn")
+var obstaculo2 = preload("res://microjogos/2023S1/projeto_vinicius_carvalho/cenas/obstaculo_2.tscn")
+var obstaculo3 = preload("res://microjogos/2023S1/projeto_vinicius_carvalho/cenas/obstaculo_3.tscn")
+var player_position
+
+func _ready():
+	som = $som_cavern
+	som.play()
+	timer = $Timer
+	timer.start(0.5)
+	timer_win = $TimersWin
+	timer_win.start(5)
+	match Global.language:
+		Global.LANGUAGE.EN:
+			NotificationCenter.notify("Survive!")
+		Global.LANGUAGE.PT:
+			NotificationCenter.notify("Sobreviva!")
+
+	
+	
+func _physics_process(delta):
+	player_position = $CharacterBody2D.position
+	$Camera2D.position.x = player_position.x
+	
+func instanciar_obj():
+	var lista = []
+	var indice =  randi() % 3
+	lista.append(obstaculo1.instantiate())
+	lista.append(obstaculo2.instantiate())
+	lista.append(obstaculo3.instantiate())
+	lista[indice].position = Vector2(0,0)
+	
+	add_child(lista[indice])
+
+
+func _on_timer_timeout():
+	timer.start(2)
+	instanciar_obj()
+
+
+func _on_timers_win_timeout():
+	if $CharacterBody2D.vivo:
+		match Global.language:
+			Global.LANGUAGE.EN:
+				NotificationCenter.notify("YOU WIN!")
+			Global.LANGUAGE.PT:
+				NotificationCenter.notify("VOCE GANHOU!")
+		register_win()
+		queue_free()
+	else:
+		pass
+
+
+func register_win():
+	emit_signal("win")
+
+
+func register_lose():
+	emit_signal("lose")
