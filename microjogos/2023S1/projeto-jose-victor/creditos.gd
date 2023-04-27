6cb3cf2adff07ec2f056f855d028d2b76433fef8
GoliasVictor <dev.jvsb@gmail.com>
2023-04-27 16:48:37 -0300

    Adicionado creditos SFX

--- microjogos/2023S1/projeto-jose-victor/creditos.gd
+++ microjogos/2023S1/projeto-jose-victor/creditos.gd
@@ -10,11 +10,12 @@ Criado por: José Victor Santana Barbosa
 Texturas usadas do jogo Sneedy de Lazy Fox Licenciadas sobre  CC BY 4.0. 
 Sons:
 	Apple 2.wav(https://freesound.org/s/218064/) de MEAXX(https://freesound.org/people/MEAXX/) sobre CC BY 4.0(https://creativecommons.org/licenses/by/4.0/)
+	SFX: https://beepyeah.itch.io/8-bit-sfx-pack
 """
 var credits_en = """
 Made by: José Victor Santana Barbosa
 Used textures from the game Sneedy by Lazy Fox Licensed under CC BY 4.0.
 Sons:
 	Apple 2.wav(https://freesound.org/s/218064/) by MEAXX(https://freesound.org/people/MEAXX/) under CC BY 4.0(https://creativecommons.org/licenses/by/4.0/)
-
+	SFX: https://beepyeah.itch.io/8-bit-sfx-pack
 """
