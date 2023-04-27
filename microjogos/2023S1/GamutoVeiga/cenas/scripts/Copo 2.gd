0b133a6055a6d7f3d1250088061d02ffcb765034
Rafael Carro Gaudim <rafaelcgaudim@yahoo.com.br>
2023-04-27 15:30:29 -0300

    Fixed some code, added sounds.
    - Added more Sounds to the game.
    - Fixed some code to get the cup to show the right cup when you lose.
    
    [WARNING] The game scene is not in the main.tscn, but in the Node2D.tscn, when implementing it in the Class of 2023, use the path for the Node2D.tscn NOT the main.tscn path.

---
+++ microjogos/2023S1/GamutoVeiga/cenas/scripts/Copo 2.gd
@@ -0,0 +1,11 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass 
