729d31294a6f8b8a14d067c27d02b22f0cc26f48
Isael <isaelgsfaria@gmail.com>
2023-06-01 02:14:39 -0300

    Created basic game folder

---
+++ microjogos/2023S1/mail_cleaner/creditos.gd
@@ -0,0 +1,23 @@
+extends Resource
+
+
+# Este arquivo vai ser usado diretamente nos créditos do jogo! Dê crédito a todas as pessoaas que
+# participaram da criação do jogo e a todos os recursos externos utilizados. Lembre que a fonte que
+# será utilizada não suporte caracteres latinos como ~ ou ´
+
+
+# Título do seu jogo
+var title_pt = "Limpador de email"
+var title_en = "Mail cleaner"
+
+# Detalhes dos créditos do seu jogo
+var credits_pt = """
+Criado por: IsaelGabriel
+
+Você pode preencher o resto deste campo com creditos de assets utilizados, links para sua redes sociais ou qualquer outro detalhe que ache relevante
+"""
+var credits_en = """
+Made by: IsaelGabriel
+
+Fill this space with credits to used assets, links to your social medias and whatever other details you think are important
+"""
