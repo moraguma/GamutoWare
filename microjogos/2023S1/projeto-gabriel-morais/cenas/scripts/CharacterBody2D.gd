11830ecf7b4bb80a32e444dd68818b7234def3ad
Moraguma <g170603@dac.unicamp.br>
2023-08-10 14:02:05 -0300

    Better game feel

--- microjogos/2023S1/projeto-gabriel-morais/cenas/scripts/CharacterBody2D.gd
+++ microjogos/2023S1/projeto-gabriel-morais/cenas/scripts/CharacterBody2D.gd
@@ -4,21 +4,20 @@ var foi_jogado = 0
 const SPEED = 400.0
 const JUMP_VELOCITY = -400.0
 
+const ELASTIC_CONSTANT = 200
+const MAX_DIST = 200
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = 2000
+
+
+@onready var pivot = $"../pivo"
+
 
 func _ready():
 	position = Vector2(540, 800)
 
 func _physics_process(delta):
-	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity.y += gravity * delta
-
-	# Handle Jump.
-#	if Input.is_action_just_pressed("ui_accept"):
-#		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	if foi_jogado == 0:
@@ -38,17 +37,19 @@ func _physics_process(delta):
 			direction_x = 0
 		var direction = Vector2(direction_x, direction_y).normalized()
 		velocity = direction * SPEED
+		
+		var to_pivot = pivot.position - position
+		var distance = max(0, (position - pivot.position).length() - MAX_DIST)
+		velocity += to_pivot.normalized() * max(0, (position - pivot.position).length() - MAX_DIST) * ELASTIC_CONSTANT * delta
 	else:
 		velocity.y += gravity * delta
 	if Input.is_action_pressed("acao") and foi_jogado==0:
 		velocity = 10*(get_node("../pivo").position-position)
 		get_node("tiro").play()
-		#som_jogar.play()
 		foi_jogado = 1
 	if position.x > 2000 or position.x < -300 or position.y < 0 or position.y > 2000:
 		position = Vector2(540, 800)
 		foi_jogado = 0
-		
 		
 	move_and_slide()
 
