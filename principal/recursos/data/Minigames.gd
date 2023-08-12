5de2b3fb2b310454a2409352c2ca60fb68877bfa
Isael <isaelgsfaria@gmail.com>
2023-08-12 15:32:34 -0300

    removed txts from minigame folder / removed changes to Minigames.gd

--- principal/recursos/data/Minigames.gd
+++ principal/recursos/data/Minigames.gd
@@ -2,11 +2,11 @@ extends Resource
 
 
 const minigame_packs = {
-	"Class of 2023": ["res://microjogos/2023S1/projeto-enzo-bertoloti/cenas/main.tscn", "res://microjogos/2023S1/projeto_vinicius_carvalho/cenas/mapa.tscn", "res://microjogos/2023S1/projeto-tiagoperrupato/cenas/main.tscn", "res://microjogos/2023S1/projeto-guglielmo-223300/cenas/main.tscn", "res://microjogos/2023S1/bis-limao/cenas/main.tscn", "res://microjogos/2023S1/GamutoVeiga/cenas/main.tscn", "res://microjogos/2023S1/projeto-brabesf/cenas/main.tscn", "res://microjogos/2023S1/projeto-jose-victor/cenas/main.tscn", "res://microjogos/2023S1/projeto-thedeas/cenas/main.tscn", "res://microjogos/2023S1/projeto-yan-oliveira/cenas/main.tscn", "res://microjogos/2023S1/projeto_leonardo_valente/cenas/main.tscn", "res://microjogos/2023S1/projeto-pochete/cenas/main.tscn", "res://microjogos/2023S1/projeto-RafaelCarro/cenas/main.tscn", "res://microjogos/2023S1/mail_cleaner/cenas/main.tscn"],
+	"Class of 2023": ["res://microjogos/2023S1/projeto-enzo-bertoloti/cenas/main.tscn", "res://microjogos/2023S1/projeto_vinicius_carvalho/cenas/mapa.tscn", "res://microjogos/2023S1/projeto-tiagoperrupato/cenas/main.tscn", "res://microjogos/2023S1/projeto-guglielmo-223300/cenas/main.tscn", "res://microjogos/2023S1/bis-limao/cenas/main.tscn", "res://microjogos/2023S1/GamutoVeiga/cenas/main.tscn", "res://microjogos/2023S1/projeto-brabesf/cenas/main.tscn", "res://microjogos/2023S1/projeto-jose-victor/cenas/main.tscn", "res://microjogos/2023S1/projeto-thedeas/cenas/main.tscn", "res://microjogos/2023S1/projeto-yan-oliveira/cenas/main.tscn", "res://microjogos/2023S1/projeto_leonardo_valente/cenas/main.tscn", "res://microjogos/2023S1/projeto-pochete/cenas/main.tscn", "res://microjogos/2023S1/projeto-RafaelCarro/cenas/main.tscn"],
 	"Class of 2022": ["res://microjogos/2022S1/baleia/cenas/baleiaGame.tscn", "res://microjogos/2022S1/gamuturai/cenas/main.tscn", "res://microjogos/2022S1/race/cenas/main.tscn"],
 	"Demo": ["res://microjogos/demo/tiro_ao_alvo/cenas/main.tscn", "res://microjogos/demo/frogger/cenas/main.tscn"],
 	"Godotware": ["res://microjogos/godotware/Bang/Main.tscn", "res://microjogos/godotware/Simon/Main.tscn"],
-	"Moraguma": ["res://microjogos/moraguma/candle prick/cenas/Main.tscn", "res://microjogos/moraguma/deerskull/cenas/Main.tscn", "res://microjogos/moraguma/hora_da_necromancia/cenas/Main.tscn", "res://microjogos/moraguma/ifuv/cenas/Main.tscn"],
+	"Moraguma": ["res://microjogos/moraguma/candle prick/cenas/Main.tscn", "res://microjogos/moraguma/deerskull/cenas/Main.tscn", "res://microjogos/moraguma/hora_da_necromancia/cenas/Main.tscn", "res://microjogos/moraguma/ifuv/cenas/Main.tscn"]
 }
 
 # Removed from Moraguma
@@ -43,8 +43,7 @@ const credit_paths = {
 	"res://microjogos/moraguma/hora_da_necromancia/cenas/Main.tscn": "res://microjogos/moraguma/hora_da_necromancia/creditos.gd",
 	"res://microjogos/moraguma/ifuv/cenas/Main.tscn": "res://microjogos/moraguma/ifuv/creditos.gd",
 	"res://microjogos/moraguma/soulchain/cenas/Main.tscn": "res://microjogos/moraguma/soulchain/creditos.gd",
-	"res://microjogos/moraguma/whale_eater/cenas/Main.tscn": "res://microjogos/moraguma/whale_eater/creditos.gd",
-	"res://microjogos/2023S1/mail_cleaner/cenas/main.tscn": "res://microjogos/2023S1/mail_cleaner/creditos.gd"
+	"res://microjogos/moraguma/whale_eater/cenas/Main.tscn": "res://microjogos/moraguma/whale_eater/creditos.gd"
 }
 
 const cover_paths = {
@@ -74,6 +73,5 @@ const cover_paths = {
 	"res://microjogos/moraguma/hora_da_necromancia/cenas/Main.tscn": "res://microjogos/moraguma/hora_da_necromancia/capa.png",
 	"res://microjogos/moraguma/ifuv/cenas/Main.tscn": "res://microjogos/moraguma/ifuv/capa.png",
 	"res://microjogos/moraguma/soulchain/cenas/Main.tscn": "res://microjogos/moraguma/soulchain/capa.png",
-	"res://microjogos/moraguma/whale_eater/cenas/Main.tscn": "res://microjogos/moraguma/whale_eater/capa.png",
-	"res://microjogos/2023S1/mail_cleaner/cenas/main.tscn": "res://microjogos/2023S1/mail_cleaner/capa.png"
+	"res://microjogos/moraguma/whale_eater/cenas/Main.tscn": "res://microjogos/moraguma/whale_eater/capa.png"
 }
