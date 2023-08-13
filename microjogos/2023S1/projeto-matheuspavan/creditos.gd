1fd71294306d29db316a0d033558953297ac2730
GoliasVictor <dev.jvsb@gmail.com>
2023-08-13 01:58:49 -0300

    added credits of images

--- microjogos/2023S1/projeto-matheuspavan/creditos.gd
+++ microjogos/2023S1/projeto-matheuspavan/creditos.gd
@@ -12,12 +12,21 @@ var title_en = "Title in English"
 
 # Detalhes dos créditos do seu jogo
 var credits_pt = """
-Criado por: [Seu nome!]
-
+Criado por: Matheus Pavan
+Imagens:
+	"Wizard Pack"(https://luizmelo.itch.io/wizard-pack) por LuizMelo(https://luizmelo.itch.io/) está licenciado sobre CC0(https://creativecommons.org/publicdomain/zero/1.0/)
+	"Lightning Lines Pixel Art Effect"(https://sanctumpixel.itch.io/lightning-lines-pixel-art-effect) por sanctumpixel(https://sanctumpixel.itch.io/) 
+	"EPIC-Collection book"(https://opengameart.org/content/epic-collection-book) por MELLE(https://opengameart.org/users/melle) está licenciado sobre CC0(https://creativecommons.org/publicdomain/zero/1.0/)
+	"Painterly Spell Icons part 2"(https://opengameart.org/content/painterly-spell-icons-part-2) por eleazzaar(https://opengameart.org/users/eleazzaar) está licenciado sobre CC-BY 3.0(https://creativecommons.org/licenses/by-sa/3.0/)
 Você pode preencher o resto deste campo com creditos de assets utilizados, links para sua redes sociais ou qualquer outro detalhe que ache relevante
 """
 var credits_en = """
-Made by: [Your name!]
+Made by: Mateus Pavan
+Images: 
+	"Wizard Pack"(https://luizmelo.itch.io/wizard-pack) by LuizMelo(https://luizmelo.itch.io/) is licensed under CC0(https://creativecommons.org/publicdomain/zero/1.0/)
+	"Lightning Lines Pixel Art Effect"(https://sanctumpixel.itch.io/lightning-lines-pixel-art-effect) by sanctumpixel(https://sanctumpixel.itch.io/) 
+	"EPIC-Collection book"(https://opengameart.org/content/epic-collection-book) by MELLE(https://opengameart.org/users/melle) is licensed under CC0(https://creativecommons.org/publicdomain/zero/1.0/)
+	"Painterly Spell Icons part 2"(https://opengameart.org/content/painterly-spell-icons-part-2) by eleazzaar(https://opengameart.org/users/eleazzaar) is licensed under CC-BY 3.0(https://creativecommons.org/licenses/by-sa/3.0/)
 
 Fill this space with credits to used assets, links to your social medias and whatever other details you think are important
 """
