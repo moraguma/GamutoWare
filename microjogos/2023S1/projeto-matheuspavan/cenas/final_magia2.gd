03af1548fd1868c47370f51b16450a7c1b6fcb5d
GoliasVictor <dev.jvsb@gmail.com>
2023-08-13 05:12:39 -0300

    replaced proprietary assets

--- microjogos/2023S1/projeto-matheuspavan/cenas/final_magia2.gd
+++ microjogos/2023S1/projeto-matheuspavan/cenas/final_magia2.gd
@@ -13,6 +13,6 @@ func _process(delta):
 
 func _on_area_2d_body_entered(body):
 	print("colidiu")
-	get_parent().register_win()
+	get_parent().get_parent().register_win()
 	
 	pass # Replace with function body.
