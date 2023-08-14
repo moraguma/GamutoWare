3905c7d2741036c275e66a0a9456db7d876757f9
Moraguma <g170603@dac.unicamp.br>
2023-08-14 19:21:08 -0300

    Fixes repeated Z bug

--- microjogos/2023S1/projeto-guglielmo-223300/cenas/scripts/Teclado.gd
+++ microjogos/2023S1/projeto-guglielmo-223300/cenas/scripts/Teclado.gd
@@ -1,5 +1,5 @@
 extends Node2D
-var r = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","Y","Z"]
+var r = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","X","C","V","B","Y","Z"]
 var i = 0
 var j = 0
 var d1
