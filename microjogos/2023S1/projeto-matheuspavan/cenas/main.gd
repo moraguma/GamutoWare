7120048e334b0d8ee34f06171b9f4ee3953cd9c6
Moraguma <g170603@dac.unicamp.br>
2023-08-14 19:07:50 -0300

    Resource management

--- microjogos/2023S1/projeto-matheuspavan/cenas/main.gd
+++ microjogos/2023S1/projeto-matheuspavan/cenas/main.gd
@@ -22,9 +22,9 @@ func _ready():
 	# ser feito para vencer o jogo. A fonte usada não suporta caracteres latinos como ~ ou ´
 	match Global.language:
 		Global.LANGUAGE.EN:
-			NotificationCenter.notify("DO SOMETHING!")
+			NotificationCenter.notify("CAST A SPELL!")
 		Global.LANGUAGE.PT:
-			NotificationCenter.notify("FACA ALGO!")
+			NotificationCenter.notify("USE MAGIA!")
 
 
 # Esta função é chamada uma vez por frame e é otimizada para cálculos relacionados a física, como
