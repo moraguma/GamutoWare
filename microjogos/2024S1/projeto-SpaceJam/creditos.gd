622b37817063bdaa9a2961e14534874f036f3ba2
Olimpiada Brasileira de Informatica <obi160@guns.lab.ic.unicamp.br>
2024-03-23 16:02:55 -0300

    add projeto-SpaceJam's project

---
+++ microjogos/2024S1/projeto-SpaceJam/creditos.gd
@@ -0,0 +1,23 @@
+extends Resource
+
+
+# Este arquivo vai ser usado diretamente nos créditos do jogo! Dê crédito a todas as pessoaas que
+# participaram da criação do jogo e a todos os recursos externos utilizados. Lembre que a fonte que
+# será utilizada não suporte caracteres latinos como ~ ou ´
+
+
+# Título do seu jogo
+var title_pt = "Titulo em Portugues"
+var title_en = "Title in English"
+
+# Detalhes dos créditos do seu jogo
+var credits_pt = """
+Criado por: [Seu nome!]
+
+Você pode preencher o resto deste campo com creditos de assets utilizados, links para sua redes sociais ou qualquer outro detalhe que ache relevante
+"""
+var credits_en = """
+Made by: [Your name!]
+
+Fill this space with credits to used assets, links to your social medias and whatever other details you think are important
+"""
