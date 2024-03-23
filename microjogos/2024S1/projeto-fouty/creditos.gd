77f655dca72568da8d3bf070c642befec5a91c83
Gabriel <gabrielcmarchetti@gmail.com>
2024-03-23 19:26:04 -0300

    Make base of the game

--- microjogos/2024S1/projeto-fouty/creditos.gd
+++ microjogos/2024S1/projeto-fouty/creditos.gd
@@ -5,19 +5,24 @@ extends Resource
 # participaram da criação do jogo e a todos os recursos externos utilizados. Lembre que a fonte que
 # será utilizada não suporte caracteres latinos como ~ ou ´
 
+# CREDITS TO IMAGES:
+# supermarketBackground: https://br.pinterest.com/pin/404549979004041436/?amp_client_id=CLIENT_ID%28_%29&mweb_unauth_id=%7B%7Bdefault.session%7D%7D&simplified=true
+# Apple: https://commons.wikimedia.org/wiki/File:Applepix.svg
+# Milk: https://www.vecteezy.com/png/19040575-an-8-bit-retro-styled-pixel-art-illustration-of-chocolate-milk
+
 
 # Título do seu jogo
-var title_pt = "Titulo em Portugues"
-var title_en = "Title in English"
+var title_pt = "Gamarkets"
+var title_en = "Gamerkets"
 
 # Detalhes dos créditos do seu jogo
 var credits_pt = """
-Criado por: [Seu nome!]
+Criado por: [Fouty!]
 
 Você pode preencher o resto deste campo com creditos de assets utilizados, links para sua redes sociais ou qualquer outro detalhe que ache relevante
 """
 var credits_en = """
-Made by: [Your name!]
+Made by: [Fouty!]
 
 Fill this space with credits to used assets, links to your social medias and whatever other details you think are important
 """
