e041592b6b5db1d81d2a920637610fe66ca38c18
gui-clone <guilhermehenriquefire@gmail.com>
2024-03-24 17:40:15 -0300

    adição de minigame projeto-guilherme

---
+++ microjogos/2024S1/projeto-guilherme/cenas/scripts/AnimatedSprite2D.gd
@@ -0,0 +1,16 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	play("default")
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	
+	pass
