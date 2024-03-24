9c98c91f2cd89cc59307f3b8edbc49c3d1490707
milena sam <milenafurutashishito@gmail.com>
2024-03-24 12:17:36 -0300

    Map changes

--- microjogos/2024S1/lari-sem-cafe/scripts/Player.gd
+++ microjogos/2024S1/lari-sem-cafe/scripts/Player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animPlayer = $"AnimatedSprite2D"
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -14,14 +16,19 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if (Input.is_action_just_pressed("ui_accept") or Input.is_action_pressed("ui_up")) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animPlayer.animation
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if direction > 0:
+			animPlayer.flip_h = false
+		else:
+			animPlayer.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
