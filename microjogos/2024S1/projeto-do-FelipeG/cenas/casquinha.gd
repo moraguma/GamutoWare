a6bde6642efa1bea7be93d4ec6a3312774525e19
Felipe Gayotto Bianchessi <ra281174@paralamas.lab.ic.unicamp.br>
2024-03-24 19:40:47 -0300

    'Final'

--- microjogos/2024S1/projeto-do-FelipeG/cenas/casquinha.gd
+++ microjogos/2024S1/projeto-do-FelipeG/cenas/casquinha.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 600.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -9,15 +9,25 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	if Input.is_action_pressed("direita"):
+		velocity.x = 900
+	elif Input.is_action_pressed("esquerda"):
+		velocity.x = -900
+	else:
+		velocity.x = 0
 	# Add the gravity.
 
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 
 	move_and_slide()
+
+
+
+func _on_area_2d_body_entered(body):
+	body.velocity.y=0
+	body.freeze= true
+	body.reparent(self)
+
