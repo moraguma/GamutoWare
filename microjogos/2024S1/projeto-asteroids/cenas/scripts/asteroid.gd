f26eb43b141c7148600d306a4215d3b9eb79ec18
skypillar <ph.panta@gmail.com>
2024-03-23 23:01:47 -0300

    Add asteroids logic

---
+++ microjogos/2024S1/projeto-asteroids/cenas/scripts/asteroid.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	add_to_group("asteroids")
+	
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
