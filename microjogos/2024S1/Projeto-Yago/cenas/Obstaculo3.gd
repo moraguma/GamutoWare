916887bc9a9360f760893de5ab18808b1e5571e4
yoneey <yago.157.e.y@gmail.com>
2024-03-24 19:06:25 -0300

    Jogo do Yago

---
+++ microjogos/2024S1/Projeto-Yago/cenas/Obstaculo3.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+@onready var path_find = $"../Path2D3/PathFollow2D"
+
+const SPEED = 400.0
+
+var colision = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	
+	if colision:
+		return
+		
+	if velocity.normalized() != Vector2.ZERO: #Rotação
+		rotation = lerp_angle(rotation, deg_to_rad(90) + velocity.angle(), 0.3)
+
+	velocity = self.global_position.direction_to(path_find.global_position) * SPEED #Velocidade
+	
+	if self.global_position.distance_squared_to(path_find.global_position) < 15:
+		path_find.progress_ratio += 0.07	
+	if path_find.progress_ratio > 0.95:
+		queue_free()
+		self.position = path_find.position
+	
+	if not colision and move_and_slide():
+		NotificationCenter.notify("PERDEU PLAYBOY!")
+		colision = true
--- microjogos/2024S1/Projeto-Yago/creditos.gd
+++ microjogos/2024S1/Projeto-Yago/creditos.gd
@@ -7,17 +7,17 @@ extends Resource
 
 
 # Título do seu jogo
-var title_pt = "Titulo em Portugues"
-var title_en = "Title in English"
+var title_pt = "Lista de Compras"
+var title_en = "Shopping list"
 
 # Detalhes dos créditos do seu jogo
 var credits_pt = """
-Criado por: [Seu nome!]
+Criado por: [Yago Eizo Yonemura]
 
 Você pode preencher o resto deste campo com creditos de assets utilizados, links para sua redes sociais ou qualquer outro detalhe que ache relevante
 """
-var credits_en = """
-Made by: [Your name!]
+var credits_en = """https://www.postermywall.com/index.php/art/template/d7dfa26f8c8c759652e2521f0ecd24eb/yellow-notebook-paper-background-design-template
+Made by: [Yago Eizo Yonemura]
 
 Fill this space with credits to used assets, links to your social medias and whatever other details you think are important
 """
