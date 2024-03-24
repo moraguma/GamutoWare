655c361d5c0725de32e01d3cd804fc17366f5937
dhx13 <danielhuber13@hotmail.com>
2024-03-24 19:39:53 -0300

    Submissão minigame Daniel Huber

---
+++ microjogos/2024S1/projeto-danielh/cenas/Gamuto.gd
@@ -0,0 +1,12 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_anything_pressed():
+		$AnimatedSprite2D.play("gamuto")
---
+++ microjogos/2024S1/projeto-danielh/cenas/main.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+	pass
+	
+	
+
+	 # Replace with function body.		
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_anything_pressed():
+		$Prision.play("Prision")
+		$Gamuto.play("gamuto")
+	else:
+		$Prision.stop("Prision")
+		$Gamuto.stop("gamuto")
+		
+	
