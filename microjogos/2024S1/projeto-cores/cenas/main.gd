d7cd23dc58881e98c694c229be0ea0cab43712aa
Leonardo Carvalho de Luca <ra288820@frajola.lab.ic.unicamp.br>
2024-03-25 16:03:37 -0300

    Melhorias

--- microjogos/2024S1/projeto-cores/cenas/main.gd
+++ microjogos/2024S1/projeto-cores/cenas/main.gd
@@ -58,15 +58,13 @@ func reset():
 	right=0
 	down=0
 	tentativa=[]
+	$Sprite2D.modulate = Color(1, 1, 1, 0)
+	$Sprite2D2.modulate = Color(1, 1, 1, 1)
+	$Sprite2D3.modulate = Color(1, 1, 1, 1)
+	$Sprite2D4.modulate = Color(1, 1, 1, 1)
+	$Sprite2D5.modulate = Color(1, 1, 1, 1)
+	$Sprite2D6.modulate = Color(1, 1, 1, 1)
 
-	
-
-func developer():
-	$Cor1/Polygon2D.modulate = Color(c[1][0], c[1][1], c[1][2])
-	$Cor2/Polygon2D.modulate = Color(c[2][0], c[2][1], c[2][2])
-	$Cor3/Polygon2D.modulate = Color(c[3][0], c[3][1], c[3][2])
-	$Cor4/Polygon2D.modulate = Color(c[4][0], c[4][1], c[4][2])
-	$Cor5/Polygon2D.modulate = Color(c[5][0], c[5][1], c[5][2])
 
 func put_color(i):
 	tentativa.append(i)
@@ -82,10 +80,15 @@ func put_color(i):
 		$Cor5/Polygon2D.modulate = Color(c[i][0], c[i][1], c[i][2])
 		if tentativa==resp:
 			emit_signal("win")
-			$Sprite2D.modulate = Color(1, 1, 1, 1)
+			$Sprite2D.modulate = Color((R0+R1)/2, (G0+G1)/2, (B0+B1)/2, 1)
 			$Polygon13.modulate = Color(0,0,0,0)
 			$Polygon14.modulate = Color(0,0,0,0)
 			$Polygon15.modulate = Color(0,0,0,0)
+			$Sprite2D2.modulate = Color(1, 1, 1, 0)
+			$Sprite2D3.modulate = Color(1, 1, 1, 0)
+			$Sprite2D4.modulate = Color(1, 1, 1, 0)
+			$Sprite2D5.modulate = Color(1, 1, 1, 0)
+			$Sprite2D6.modulate = Color(1, 1, 1, 0)
 			$AudioStreamPlayer2D.play()
 		else:
 			reset()
@@ -120,29 +123,37 @@ func _unhandled_input(event):
 	if event is InputEventKey:
 		if event.pressed and event.keycode == KEY_UP and up==0:
 			$CorUp/Polygon2D.modulate = Color(c[0][0], c[0][1], c[0][2], shade)
+			#$Sprite2D2.modulate = Color(c[0][0], c[0][1], c[0][2], shade)
+			$Sprite2D2.modulate = Color(1, 1, 1, 0)
 			up=up+1
 			put_color(0)
 		elif event.pressed and event.keycode == KEY_LEFT and left==0:
 			$CorLeft/Polygon2D.modulate = Color(c[1][0], c[1][1], c[1][2], shade)
+			#$Sprite2D3.modulate = Color(c[1][0], c[1][1], c[1][2], shade)
+			$Sprite2D3.modulate = Color(1, 1, 1, 0)
 			left=left+1
 			put_color(1)
 		elif event.pressed and event.keycode == KEY_SPACE and space==0:
 			$CorSpace/Polygon2D.modulate = Color(c[2][0], c[2][1], c[2][2], shade)
+			#$Sprite2D4.modulate = Color(c[2][0], c[2][1], c[2][2], shade)
+			$Sprite2D4.modulate = Color(1, 1, 1, 0)
 			space=space+1
 			put_color(2)
 		elif event.pressed and event.keycode == KEY_RIGHT and right==0:
 			$CorRight/Polygon2D.modulate = Color(c[3][0], c[3][1], c[3][2], shade)
+			#$Sprite2D5.modulate = Color(c[3][0], c[3][1], c[3][2], shade)
+			$Sprite2D5.modulate = Color(1, 1, 1, 0)
 			right=right+1
 			put_color(3)
 		elif event.pressed and event.keycode == KEY_DOWN and down==0:
 			$CorDown/Polygon2D.modulate = Color(c[4][0], c[4][1], c[4][2], shade)
+			#$Sprite2D6.modulate = Color(c[4][0], c[4][1], c[4][2], shade)
+			$Sprite2D6.modulate = Color(1, 1, 1, 0)
 			down=down+1
 			put_color(4)
 		elif event.pressed and event.keycode == KEY_R:
 			reset()
 			used=used+1
-		elif event.pressed and event.keycode == KEY_D:
-			developer()
 
 
 
