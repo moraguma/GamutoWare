45b24ea2d98396849f1cf584f8864a3530a5ce31
clara <mcmo@protonmail.com>
2024-03-24 21:56:20 -0300

    patinho

--- microjogos/2024S1/projeto-estrela/cenas/scripts/CharacterBody2D.gd
+++ microjogos/2024S1/projeto-estrela/cenas/scripts/CharacterBody2D.gd
@@ -5,25 +5,36 @@ const SPEED = 1000.0
 const JUMP_VELOCITY = -400.0
 var c=0
 var lider=self
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+
 
 
 func _physics_process(delta):
+	
 
 	var directionx = Input.get_axis("ui_left", "ui_right")
 	if directionx:
+		var direita = get_node("direitaP")
+		direita.play()
 		velocity.x = directionx * SPEED
 	else:
+		var esquerda = get_node("esquerdaP")
+		esquerda.play()
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	var directiony = Input.get_axis("ui_up", "ui_down")
 	if directiony:
+		var cima = get_node("cimaP")
+		cima.play()
 		velocity.y = directiony * SPEED
 	else:
+		var baixo = get_node("baixoP")
+		baixo.play()
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
 
 	move_and_slide()
 
 func coletar(body):
+	
 	c=c+1
 	return c
