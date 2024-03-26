e7e8bf10f06dbf20dbb3cf92e28359ef0684937e
onorio <onoriofelipe7@gmail.com>
2024-03-25 21:24:32 -0300

    Add spritesheets and changing cell via cursor

--- microjogos/2024S1/projeto_felipe_onorio/cenas/area_cell_0.gd
+++ microjogos/2024S1/projeto_felipe_onorio/cenas/area_cell_0.gd
@@ -9,24 +9,45 @@ extends Area2D
 @export var up_neighbor: Node = null
 @export var down_neighbor: Node = null
 
-enum State {EMPTY, FIRST, SECOND, THIRD }
+enum State {FIRST, SECOND, THIRD }
 
-var state : State = State.EMPTY
+var state : State = State.FIRST
 var sprite_animation: Node2D
-var animation_watery = preload(^"res://microjogos/2024S1/projeto_felipe_onorio/cenas/watery.tres")
-var animation_flame_fluffy = preload(^"res://microjogos/2024S1/projeto_felipe_onorio/cenas/flame_fluffy.tres")
-var animation_flame_pixely = preload(^"res://microjogos/2024S1/projeto_felipe_onorio/cenas/flame_pixely.tres")
-var animation_leaf = preload(^"res://microjogos/2024S1/projeto_felipe_onorio/cenas/leaf.tres")
+var animation_watery = preload("res://microjogos/2024S1/projeto_felipe_onorio/cenas/watery.tres")
+var animation_flame_fluffy = preload("res://microjogos/2024S1/projeto_felipe_onorio/cenas/flame_fluffy.tres")
+var animation_flame_pixely = preload("res://microjogos/2024S1/projeto_felipe_onorio/cenas/flame_pixely.tres")
+var animation_leaf = preload("res://microjogos/2024S1/projeto_felipe_onorio/cenas/leaf.tres")
 #export var player_path := @""; onready var player := get_node(player_path) as Node2D
 
+func change_next_state():
+	#print("Current state from cell_0: ", state)
+	state = (state + 1) % 3
+	#print("State from cell_0 changed to: ", state)
+	match state:
+		State.FIRST:
+			sprite_animation.set_sprite_frames(animation_flame_fluffy)
+		State.SECOND:
+			sprite_animation.set_sprite_frames(animation_watery)
+		State.THIRD:
+			sprite_animation.set_sprite_frames(animation_leaf)
+	sprite_animation.play("default")
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#right_neighbor = get_node(right_neighbor_path)
 	#down_neighbor = get_node(^"../area_cell_3")
 	sprite_animation = get_child(0).get_child(0)
-	sprite_animation.set_animation(animation_watery)
+	#sprite_animation.set_animation(animation_watery)
+	#sprite_animation.set_animation("res://microjogos/2024S1/projeto_felipe_onorio/cenas/watery.tres")
+	sprite_animation.set_sprite_frames(animation_watery)
+	sprite_animation.play("default")
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_cursor_area_activate_cell():
+	change_next_state()
+	pass # Replace with function body.
--- microjogos/2024S1/projeto_felipe_onorio/cenas/cursor_area.gd
+++ microjogos/2024S1/projeto_felipe_onorio/cenas/cursor_area.gd
@@ -3,9 +3,21 @@ extends Area2D
 @export var current_cell: Node
 var counter: int = 0
 
+signal activate_cell()
+
+# syntax for dynamic connection: signalling_node.connect("signal_name", receiving_node, "signal_handler")
+
+func disconnect_signal_handler():
+	activate_cell.disconnect(current_cell._on_cursor_area_activate_cell)
+
+func connect_signal_handler():
+	activate_cell.connect(current_cell._on_cursor_area_activate_cell)
+
 func move_right():
 	if current_cell.right_neighbor != null:
+		#current_cell.disconnect()
 		current_cell = current_cell.right_neighbor
+		#activate_cell.connect(current_cell._on_cursor_area_activate_cell)
 	else:
 		#todo: fun visual effect
 		print("out of bounds: right")
@@ -30,15 +42,16 @@ func move_down():
 	else:
 		#todo: fun visual effect
 		print("out of bounds: down")
+		pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#current_cell  = get_node(^"../area_cell_0")
-	print("AAAAAAAAAAAAAAAA")
+	#print("AAAAAAAAAAAAAAAA")
 	#position = Vector2(222,111)
-	print("BBBBBBBBBBBBB")
+	#print("BBBBBBBBBBBBB")
 	#var misc_position = current_cell.position
-	print("CCCCCCCCCCC")
+	#print("CCCCCCCCCCC")
 	#position = misc_position
 	pass # Replace with function body.
 
@@ -58,13 +71,25 @@ func _process(delta):
 func _on_control_xxxx():
 	pass # Replace with function body.
 
+func activate_current_cell():
+	activate_cell.emit()
+
 func _on_control_right_pressed():
+	disconnect_signal_handler()
 	move_right()
+	connect_signal_handler()
 func _on_control_left_pressed():
+	disconnect_signal_handler()
 	move_left()
+	connect_signal_handler()
 func _on_control_up_pressed():
+	disconnect_signal_handler()
 	move_up()
+	connect_signal_handler()
 func _on_control_down_pressed():
+	disconnect_signal_handler()
 	move_down()
+	connect_signal_handler()
 func _on_control_space_pressed():
-	print("acao handled in cursor with no effect")
+	activate_current_cell()
+	#print("acao handled in cursor with no effect")
