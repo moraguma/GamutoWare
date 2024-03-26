95f50acea2fe7549122f53ec392d6165f439f615
estrela-16 <164669306+estrela-16@users.noreply.github.com>
2024-03-26 17:11:35 -0300

    patinhos

--- microjogos/2024S1/projeto-estrela/cenas/scripts/CharacterBody2D.gd
+++ microjogos/2024S1/projeto-estrela/cenas/scripts/CharacterBody2D.gd
@@ -7,34 +7,42 @@ var c=0
 var lider=self
 
 
+func _process(_delta):
+	var left = Input.is_action_pressed("esquerda")
+	var right = Input.is_action_pressed("direita")
+	var up = Input.is_action_pressed("cima")
+	var down = Input.is_action_pressed("baixo")
+	
+	if right:
+		$nadar.play("direita")
+		
+	elif left:
+		$nadar.play("esquerda")
+		
+	elif up:
+		$nadar.play("cima")
+		
+	elif down:
+		$nadar.play("baixo")
+
 
 func _physics_process(delta):
 	
-
-	var directionx = Input.get_axis("ui_left", "ui_right")
-	if directionx:
-		var direita = get_node("direitaP")
-		direita.play()
-		velocity.x = directionx * SPEED
-	else:
-		var esquerda = get_node("esquerdaP")
-		esquerda.play()
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	var directiony = Input.get_axis("ui_up", "ui_down")
-	if directiony:
-		var cima = get_node("cimaP")
-		cima.play()
-		velocity.y = directiony * SPEED
-	else:
-		var baixo = get_node("baixoP")
-		baixo.play()
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-		
-
+	var directionx = Input.get_axis("ui_left","ui_right")
+	var directiony = Input.get_axis("ui_up","ui_down")
+	velocity.y = directiony * SPEED
+	velocity.x = directionx * SPEED
+	#velocity.y = move_toward(velocity.y, 0, SPEED)
+	#velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 	move_and_slide()
 
 func coletar(body):
-	
+	$quack.play()
 	c=c+1
+	if c>=5:
+		get_parent().register_win() # Volta para o Node principal e chama a funcao register_win enviando o sinal para o GamutoWare
 	return c
+	
+
+
--- microjogos/2024S1/projeto-estrela/cenas/scripts/patinho.gd
+++ microjogos/2024S1/projeto-estrela/cenas/scripts/patinho.gd
@@ -8,8 +8,16 @@ func _ready():
 	pass 
 func _process(delta):
 	if pato!=null:
-		if (pato.position-position).length()>200:
+		if (pato.position-position).length()>190:
 			var m=(pato.position-position).normalized()
+			if m[0] < 0:
+				$moves.play("esquerda")
+			elif m[0] > 0:
+				$moves.play("direita")
+			elif m[1] > 0:
+				$moves.play("cima")
+			elif m[1] < 0:
+				$moves.play("baixo")
 			position+=m*v*delta
 					
 func _on_body_entered(body):
