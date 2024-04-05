94a3a4018e086dfb2209e278c99b2f544db94b9a
Gabriel Soares <g.soares@ifpeopensource.com.br>
2024-04-05 14:44:03 -0300

    Fix loading bugs

--- principal/cenas/scripts/Jogo.gd
+++ principal/cenas/scripts/Jogo.gd
@@ -179,7 +179,7 @@ func _ready():
 					microgame_queue = microgame_queue.slice(0, i) + \
 						microgame_queue.slice(i + 1, minigame_data[microgame_queue[i]]["difficulty"]) + \
 						[microgame_queue[i]] + microgame_queue.slice(minigame_data[microgame_queue[i]]["difficulty"])
-	for i in range(2):
+	for i in range(min(2,len(microgame_queue))):
 		ResourceLoader.load_threaded_request(microgame_queue[i])
 		
 	display_icons()
