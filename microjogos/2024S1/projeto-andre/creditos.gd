936fb6aacf0c4a850f0eff6c55df9e7f92bcae7a
Moraguma <g170603@dac.unicamp.br>
2024-04-11 19:37:15 -0300

    Adds sfx

--- microjogos/2024S1/projeto-andre/creditos.gd
+++ microjogos/2024S1/projeto-andre/creditos.gd
@@ -16,12 +16,16 @@ Criado por: André Amadeu Satorres
 
 Adivinhe se sua carta vence ou não o jogo!
 
+SFX de Kenney.nl
+
 Baseado em fodinha. Você pode ler mais sobre as regras do jogo em https://ludopedia.com.br/jogo/fodinha
 """
 var credits_en = """
 Made by: André Amadeu Satorres
 
 Guess whether or not your card wins the game!
+
+SFX from Kenney.nl
 
 Based on fodinha. You can read more about the rules of the game in https://ludopedia.com.br/jogo/fodinha
 """
