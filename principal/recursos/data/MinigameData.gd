53d3d93e4a83fd5219968479b4e6257c906962c7
IsaelGabriel <isaelgabrielfaria@gmail.com>
2024-04-21 20:19:11 -0300

    Update minigame data resource

--- principal/recursos/data/MinigameData.gd
+++ principal/recursos/data/MinigameData.gd
@@ -1,1 +1,14 @@
+@tool
 extends Resource
+
+class_name MinigameData
+
+@export var main_path: PackedScene
+@export var credits_path: Script
+@export var cover: Texture2D
+
+var difficulty = 0
+
+func _process(_delta):
+	if Engine.is_editor_hint():
+		pass
