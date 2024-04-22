edf7636e20a9b8c128b0d773c7d461f1b7956571
IsaelGabriel <isaelgabrielfaria@gmail.com>
2024-04-22 00:25:26 -0300

    Add Minigame Data resource code

--- principal/recursos/data/MinigameData.gd
+++ principal/recursos/data/MinigameData.gd
@@ -3,12 +3,35 @@ extends Resource
 
 class_name MinigameData
 
-@export var main_path: PackedScene
-@export var credits_path: Script
-@export var cover: Texture2D
+enum MinigameFilter {
+	MATURE,
+	NO_LUCK
+}
 
+@export_category("Informações | Info")
+@export var main_scene: PackedScene :
+	set(value):
+		if value == null: 
+			assert(value != null,"Ué")
+			push_warning("Main Scene can't be null.")
+		main_scene = value
+@export var cover: Texture2D
+@export_flags("+13", "No Luck") var filters = 0
 var difficulty = 0
 
-func _process(_delta):
-	if Engine.is_editor_hint():
-		pass
+@export_category("Créditos | Credits")
+@export_group("Português")
+@export var title_pt: StringName
+@export_multiline var credits_pt := ""
+
+@export_group("English")
+@export var title_en: StringName
+@export_multiline var credits_en := ""
+
+# Returns an Array containing the minigame filters
+func get_filter_list() -> Array[MinigameFilter]:
+	var list = []
+	for filter in MinigameFilter.values():
+		if (filters >> filter) & 1:
+			list.append(MinigameFilter[filter])
+	return list
