5527c4f4026a8c1b46896638c9311801f4c8fe4a
ayudaV <teradaandre@gmail.com>
2024-05-16 17:46:22 -0300

    projeto-de-guilherme

---
+++ microjogos/2024S1/projeto-guilherme/creditos.gd
@@ -0,0 +1,40 @@
+extends Resource
+
+
+# Este arquivo vai ser usado diretamente nos créditos do jogo! Dê crédito a todas as pessoaas que
+# participaram da criação do jogo e a todos os recursos externos utilizados. Lembre que a fonte que
+# será utilizada não suporte caracteres latinos como ~ ou ´
+
+
+# Título do seu jogo
+<<<<<<<< HEAD:microjogos/2024S1/Projeto-Yago/creditos.gd
+var title_pt = "Lista de Compras"
+var title_en = "Shopping list"
+
+# Detalhes dos créditos do seu jogo
+var credits_pt = """
+Criado por: [Yago Eizo Yonemura]
+========
+var title_pt = "Hora do hambúrguer"
+var title_en = "Burger time"
+
+# Detalhes dos créditos do seu jogo
+var credits_pt = """
+Criado por: Guilherme Henrique da Silva
+
+fonte obtida em: https://www.dafont.com/pt/dogica.font
+>>>>>>>> 046474ad8afd13b6fea7ff38677c65f3e324eedd:microjogos/2024S1/projeto-guilherme/creditos.gd
+
+"""
+<<<<<<<< HEAD:microjogos/2024S1/Projeto-Yago/creditos.gd
+var credits_en = """https://www.postermywall.com/index.php/art/template/d7dfa26f8c8c759652e2521f0ecd24eb/yellow-notebook-paper-background-design-template
+Made by: [Yago Eizo Yonemura]
+========
+var credits_en = """
+Made by: Guilherme Henrique da Silva
+
+font obtained from: https://www.dafont.com/pt/dogica.font
+
+>>>>>>>> 046474ad8afd13b6fea7ff38677c65f3e324eedd:microjogos/2024S1/projeto-guilherme/creditos.gd
+
+"""
