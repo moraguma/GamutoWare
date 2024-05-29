1ae642eccb777f61ea1aa7ecc24935b05c4a2731
ayudaV <teradaandre@gmail.com>
2024-05-29 19:13:26 -0300

    add projeto-fakarasz

--- microjogos/2024S1/Projeto-FaKarasz/creditos.gd
+++ microjogos/2024S1/Projeto-FaKarasz/creditos.gd
@@ -7,17 +7,17 @@ extends Resource
 
 
 # Título do seu jogo
-var title_pt = "Titulo em Portugues"
-var title_en = "Title in English"
+var title_pt = "Penalty"
+var title_en = "Penalty"
 
 # Detalhes dos créditos do seu jogo
 var credits_pt = """
-Criado por: [Seu nome!]
+Criado por: [FaKarasz!]
 
 Você pode preencher o resto deste campo com creditos de assets utilizados, links para sua redes sociais ou qualquer outro detalhe que ache relevante
 """
 var credits_en = """
-Made by: [Your name!]
+Made by: [FaKarasz!]
 
 Fill this space with credits to used assets, links to your social medias and whatever other details you think are important
 """
