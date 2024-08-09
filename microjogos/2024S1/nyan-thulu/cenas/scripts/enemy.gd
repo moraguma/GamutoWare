9d75bea8488953a3369335a1709ac3aa3a90a3d1
moraguma <g170603@dac.unicamp.br>
2024-08-09 15:16:09 -0300

    Lowers speed of vertical cookies

--- microjogos/2024S1/nyan-thulu/cenas/scripts/enemy.gd
+++ microjogos/2024S1/nyan-thulu/cenas/scripts/enemy.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 800.0 
+const V_SPEED = 600.0
+const H_SPEED = 800.0 
 @export var direction = Vector2(0,1)
 
 func _physics_process(delta):
 	# Add the gravity.
 	var player = self.get_parent().get_node("Player")
 	direction = self.global_position.direction_to(player.global_position)
-	velocity =  SPEED * direction 
+	velocity =  (H_SPEED if direction[0] != 0 else V_SPEED) * direction 
 	move_and_slide()
