1d7ef7386adbe27421497f18eb06e6bafdeb8364
Bernardo Arantes <ra247125@zero.lab.ic.unicamp.br>
2025-04-05 16:38:09 -0300

    Criei meu projeto.

---
+++ microjogos/2025S1/projeto_bernardoa/cenas/scripts/main.gd
@@ -0,0 +1,72 @@
+extends Node2D
+
+# Declaração dos sinais win e lose
+
+signal win
+signal lose
+
+# Estas constantes são usadas para determinar o tamanho da tela do seu jogo. Por padrão, definem uma
+# tela 1920x1080, que é padrão para monitores full HD. Caso você queira uma resolução menor para 
+# atingir uma estética mais pixelada, você pode mudar estes números para qualquer outra resolução 
+# 16:9
+const WIDTH = 1920
+const HEIGHT = 1080
+
+
+# --------------------------------------------------------------------------------------------------
+# FUNÇÕES PADRÃO
+# --------------------------------------------------------------------------------------------------
+
+# Esta função é chamada assim que esta cena é instanciada, ou seja, assim que seu minigame inicia
+func _ready():
+	# Verifica a linguagem do jogo e mostra texto nesta linguagem. Deve dar uma ideia do que deve
+	# ser feito para vencer o jogo. A fonte usada não suporta caracteres latinos como ~ ou ´
+	match Global.language:
+		Global.LANGUAGE.EN:
+			NotificationCenter.notify("DO SOMETHING!")
+		Global.LANGUAGE.PT:
+			NotificationCenter.notify("FAÇA ALGO!")
+
+
+# Esta função é chamada uma vez por frame e é otimizada para cálculos relacionados a física, como
+# a movimentação de um personagem. O parâmetro delta indica a quantidade de tempo que passou desde
+# a última chamada desta função. O comando pass não faz nada
+func _physics_process(delta):
+	pass
+
+
+# Esta função é chamada uma vez por frame e é otimizada para cálculos relacionados a renderização, 
+# como a movimentação de um personagem. O parâmetro delta indica a quantidade de tempo que passou 
+# desde a última chamada desta função. O comando pass não faz nada
+func _process(delta):
+	pass
+
+
+# --------------------------------------------------------------------------------------------------
+# SUAS FUNÇÕES
+# --------------------------------------------------------------------------------------------------
+
+
+# Um método genérico. Crie quantos métodos você precisar!
+func my_method():
+	pass
+
+
+# --------------------------------------------------------------------------------------------------
+# CONDIÇÕES DE VITÓRIA
+# --------------------------------------------------------------------------------------------------
+# Quando o jogo começa, ela assume que o jogador não conseguiu vencer o jogo ainda, ou seja, se não
+# acontecer nada, o jogador vai perder o jogo. A verificação se o jogador venceu o minigame é feita
+# com base na emissão dos sinais "win" e "lose". Se "win" foi o último sinal emitido, o jogador
+# vencerá o jogo, e se "lose" foi o último sinal emitido ou nenhum sinal foi emitido, o jogador
+# perderá o jogo
+
+
+# Chame esta função para registrar que o jogador venceu o jogo
+func register_win():
+	emit_signal("win")
+
+
+# Chame esta função para registrar que o jogador perdeu o jogo
+func register_lose():
+	emit_signal("lose")
