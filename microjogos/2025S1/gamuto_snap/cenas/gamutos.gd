2c8479839ab91a18022e47e4861937a22b642161
4wardAerial <4lucas.h.bertanha8@gmail.com>
2025-04-06 00:46:16 -0300

    Adiciona mais instâncias de gamutinhos na cena main

--- microjogos/2025S1/gamuto_snap/cenas/gamutos.gd
+++ microjogos/2025S1/gamuto_snap/cenas/gamutos.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 @onready var shader : ShaderMaterial = $Sprite2D.material
 
+# Adiciona a outline quando estiver no foco do jogador
 func _on_area_entered(area: Area2D) -> void:
 	shader.set_shader_parameter("width", 1.0)
 
+# Remove a outline depois de sair do foco do jogador
 func _on_area_exited(area: Area2D) -> void:
 	shader.set_shader_parameter("width", 0.0)
