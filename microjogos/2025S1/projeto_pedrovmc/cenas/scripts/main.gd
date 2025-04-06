830b7151dd5084c8a95a3ed1e7c45c1562103019
Pedrovmc04 <pedrovmc16@gmail.com>
2025-04-06 19:05:08 -0300

    Finalizando Projeto-Pedrovmc

--- microjogos/2025S1/projeto_pedrovmc/cenas/scripts/main.gd
+++ microjogos/2025S1/projeto_pedrovmc/cenas/scripts/main.gd
@@ -17,15 +17,37 @@ const HEIGHT = 1080
 # FUNÇÕES PADRÃO
 # --------------------------------------------------------------------------------------------------
 
+var rng = RandomNumberGenerator.new()
+var enemy_to_shoot
+
 # Esta função é chamada assim que esta cena é instanciada, ou seja, assim que seu minigame inicia
 func _ready():
 	# Verifica a linguagem do jogo e mostra texto nesta linguagem. Deve dar uma ideia do que deve
 	# ser feito para vencer o jogo. A fonte usada não suporta caracteres latinos como ~ ou ´
 	match Global.language:
 		Global.LANGUAGE.EN:
-			NotificationCenter.notify("DO SOMETHING!")
+			NotificationCenter.notify("PROTECT YOURSELF!!")
 		Global.LANGUAGE.PT:
-			NotificationCenter.notify("FAÇA ALGO!")
+			NotificationCenter.notify("SE PROTEJA!!")
+	
+	enemy_to_shoot = round(rng.randf_range(0,3))
+	
+	$EnemyLeft.setDirecao("Left")
+	$EnemyRight.setDirecao("Right")
+	$EnemyDown.setDirecao("Down")
+	$EnemyUp.setDirecao("Up")
+	
+	await get_tree().create_timer(1).timeout
+	
+	match enemy_to_shoot :
+		0.0:
+			$EnemyLeft.shoot()
+		1.0:
+			$EnemyUp.shoot()
+		2.0:
+			$EnemyRight.shoot()
+		3.0:
+			$EnemyDown.shoot()
 
 
 # Esta função é chamada uma vez por frame e é otimizada para cálculos relacionados a física, como
@@ -39,6 +61,15 @@ func _physics_process(delta):
 # como a movimentação de um personagem. O parâmetro delta indica a quantidade de tempo que passou 
 # desde a última chamada desta função. O comando pass não faz nada
 func _process(delta):
+	if Input.is_action_just_pressed("baixo"):
+		$Player.attack("Down")
+	elif Input.is_action_just_pressed("cima"):
+		$Player.attack("Up")
+	elif Input.is_action_just_pressed("direita"):
+		$Player.attack("Right")
+	elif Input.is_action_just_pressed("esquerda"):
+		$Player.attack("Left")
+	
 	pass
 
 
