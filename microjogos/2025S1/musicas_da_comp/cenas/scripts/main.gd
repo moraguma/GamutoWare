3669fa94bb10cd130654dbeeb5b858eb50da327d
Caio Lima Albuquerque <c288808@dac.unicamp.br>
2025-04-06 00:10:57 -0300

    Termina as mensagens das perguntas e das possíveis respostas para cada uma

--- microjogos/2025S1/musicas_da_comp/cenas/scripts/main.gd
+++ microjogos/2025S1/musicas_da_comp/cenas/scripts/main.gd
@@ -12,13 +12,13 @@ signal lose
 const WIDTH = 1920
 const HEIGHT = 1080
 
-const perguntas_Corpo_Nu = []
+const perguntas_Corpo_Nu = ["Sua mãe ... querendo me matar. Pode deixar minha sogra eu penso em me casar", "Me chamam de ... não quero nem saber, eu sei que estou ... por você", "Alô galera, eu ... agora sou um rapaz sério, muito sério", "Você é minha paixão, chora não, porquê eu te amo Assinado simplesmente: ..."]
 const perguntas_Renata = ["Por ela fui fiel, tão cego eu fiquei ... amigos eu deixei", "Renata ingrata, trocou meu amor por ...", "A lua entristeceu, o céu mudou de cor, Renata foi embora e ...", "Foi irracional ... mas vou deletar, ah, ah, sua insentatez"]
 const perguntas_Ragatanga = ["E o DJ que já conhece ... pra Diego, a canção mais desejada", "Aserehe ra de re De hebe tu de hebere seibiunouba mahabi ...", "Não é por acaso que encontro todo dia ... caminhando", "Olha lá quem vem virando a esquina vem ... com toda a alegria, festejando"]
 const perguntas_Madagascar = ["Ela é tão tudo! Ela é tão tudo! Tudo que eu queria ...", "Cabelo colorido, de tomara que caia, meia preta arrastão pra combinar com ...", "Melhor exposição que tenho ... toda delicadinha usando óculos grande", "Vem comigo, amor, eu vou levar pra passear de mãos dadas na areia ..."]
 
 #Dicionários para salvar as perguntas de cada música
-const corpo_Nu = {}
+const corpo_Nu = {perguntas_Corpo_Nu[0]: ["Bolada", "Com raiva", "Emburrada", "Preocupada"], perguntas_Corpo_Nu[1]: ["Maluco... louco, louco", "Louco... maluco, maluco", "Maluco... maluco, louco", "Louco... louco, maluco"], perguntas_Corpo_Nu[2]: ["Parei de zoar", "Cansei de zuar", "Mandei pra lá", "Achei ela"], perguntas_Corpo_Nu[3]: ["Fleury 02", "Vinicius e Andinho", "Piauí", "DJ Marlboro"]}
 const renata = {perguntas_Renata[0]: ["E no night futebol", "Idas para o futebol", "E nas night, futebol", "Indo para o futebol"], perguntas_Renata[1]: ["Uma ilusão", "Um pedaço de pão", "Um garanhão", "Uma sensação"], perguntas_Renata[2]: ["A deprê ficou", "A tevê levou", "Junto o meu amor", "Fez um favor"], perguntas_Renata[3]: ["O que ela fez", "O que você fez", "Como toda vez", "Mais uma vez"]}
 const ragatanga = {perguntas_Ragatanga[0]: ["Toca o som da meia-noite", "Vai tocando toda a noite", "Vai dançando lá no front", "Dedica à meia-noite"], perguntas_Ragatanga[1]: ["An de bugui an de buididipi", "To the rhythm of the boogi, the beat", "Andebuggy Andebuggy Riby", "And the boogie and the boogie, the beat"], perguntas_Ragatanga[2]: ["Cuando me voy", "Quando eu vou", "Quanto eu tô", "Enquando eu vou"], perguntas_Ragatanga[3]: ["Diego", "Fernando", "Hermano", "Miguel"]}
 const madagascar = {perguntas_Madagascar[0]: ["Abraçar, beijar", "Era abraçar beijar", "Era poder beijar", "Amassar, beijar"], perguntas_Madagascar[1]: ["Uma saia", "A saia", "O Saia", "A sala"], perguntas_Madagascar[2]: ["No meu stand", "Na minha estante", "Naquele instante", "No mesmo instante"], perguntas_Madagascar[3]: ["De madagascar", "Da beira mar", "Do lado de lá", "No nosso lar"]}
