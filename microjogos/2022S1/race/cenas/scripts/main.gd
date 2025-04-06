2095fb599a933ede268e4d9197a501a0cda7be14
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2025-04-05 22:08:10 -0300

    barra subindio e descendo

--- microjogos/2022S1/race/cenas/scripts/main.gd
+++ microjogos/2022S1/race/cenas/scripts/main.gd
@@ -10,7 +10,12 @@ const HEIGHT = 1080
 var winnable = true
 var losable = true
 
+@onready var anzol: Sprite2D = $Sprite2D
+var a_posi_inic
+
 func _ready():
+	a_posi_inic = anzol.position
+	
 	match Global.language:
 		Global.LANGUAGE.PT:
 			NotificationCenter.notify("VENCA A CORRIDA!")
@@ -18,7 +23,6 @@ func _ready():
 			NotificationCenter.notify("WIN THE RACE!")
 	
 	randomize()
-
 
 func register_win():
 	if(winnable):
@@ -32,3 +36,16 @@ func register_lose():
 		losable = false
 		get_node("DefeatSFX").play()
 		emit_signal("lose")
+		
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("baixo"):
+		queue_free()
+	if Input.is_action_just_pressed("baixo"):
+		get_tree().change_scene_to_file("res://microjogos/2022S1/Yhago_Gamelab/cenas/main.tscn")
+	
+	if Input.is_action_just_pressed("acao"):
+		anzol.position.y -= 32
+	
+	elif anzol.position != a_posi_inic:
+		anzol.position.y = lerp(anzol.position.y, a_posi_inic.y, delta)
+		
