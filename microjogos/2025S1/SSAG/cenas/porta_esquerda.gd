f600404a044c93942865fe4211c0676cb9024798
Guilherme Boarini Hodgson <ra218323@sepultura.lab.ic.unicamp.br>
2025-04-05 22:04:59 -0300

    Começo de codigo de mudar sprite da porta

--- microjogos/2025S1/SSAG/cenas/porta_esquerda.gd
+++ microjogos/2025S1/SSAG/cenas/porta_esquerda.gd
@@ -1,25 +1,10 @@
-extends CharacterBody2D
+extends Area2D
 
-var vel = 60
-var grav = -150
-
-
-func _physics_process(delta):
-	velocity.y += 5
-	if(Input.is_action_just_pressed("cima") and is_on_floor()):
-		velocity.y = grav
-	if(Input.is_action_pressed("direita")):
-		velocity.x = vel
-	else:
-		if(velocity.x < 0):
-			velocity.x += 10
-		if(velocity.x > 0):
-			velocity.x = 0
-	if(Input.is_action_pressed("esquerda")):
-		velocity.x = -vel
-	else:
-		if(velocity.x > 0):
-			velocity.x -= 10
-		if(velocity.x < 0):
-			velocity.x = 0
-	move_and_slide()
+var new_texture = preload("res://microjogos/2025S1/SSAG/ouuu.png")
+var old_texture = preload("res://microjogos/2025S1/SSAG/untitled.png")
+func _ready():
+	$portaesq.texture = new_texture
+func _process(delta: float) -> void:
+	if(Input.is_action_just_pressed("direita")):
+		$portaesq.texture = old_texture
+	
