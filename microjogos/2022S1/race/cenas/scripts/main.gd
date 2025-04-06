58a8d38a86f23997fc66d84c1d63053d006aff24
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2025-04-06 10:40:38 -0300

    barrinha movimento ajustado

--- microjogos/2022S1/race/cenas/scripts/main.gd
+++ microjogos/2022S1/race/cenas/scripts/main.gd
@@ -10,17 +10,22 @@ const HEIGHT = 1080
 var winnable = true
 var losable = true
 
-@onready var anzol: Sprite2D = $Sprite2D
-var a_posi_inic
+@onready var peixe: Area2D = $Peixe
+@onready var anzol: Area2D = $Anzol
+var a_posi_inic: Vector2
+var a_posi_now: Vector2
+
+var speed = 2.5
 
 func _ready():
 	a_posi_inic = anzol.position
+	a_posi_now = a_posi_inic
 	
 	match Global.language:
 		Global.LANGUAGE.PT:
-			NotificationCenter.notify("VENCA A CORRIDA!")
+			NotificationCenter.notify("UM PEIXE MORDEU A ISCA!")
 		Global.LANGUAGE.EN:
-			NotificationCenter.notify("WIN THE RACE!")
+			NotificationCenter.notify("A FISH!")
 	
 	randomize()
 
@@ -38,14 +43,18 @@ func register_lose():
 		emit_signal("lose")
 		
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("baixo"):
+	if Input.is_action_just_pressed("cima"):
 		queue_free()
 	if Input.is_action_just_pressed("baixo"):
 		get_tree().change_scene_to_file("res://microjogos/2022S1/Yhago_Gamelab/cenas/main.tscn")
 	
 	if Input.is_action_just_pressed("acao"):
-		anzol.position.y -= 32
-	
-	elif anzol.position != a_posi_inic:
-		anzol.position.y = lerp(anzol.position.y, a_posi_inic.y, delta)
+		var new_a_posi: Vector2
+		new_a_posi.y = anzol.position.y - 64
+		a_posi_now = new_a_posi
+		
+	if !Input.is_action_pressed("acao"):
+		a_posi_now.y = lerp(a_posi_now.y, a_posi_inic.y, delta * speed)
+	#elif anzol.position != a_posi_inic:
+	anzol.position.y = lerp(anzol.position.y, a_posi_now.y, delta * speed)
 		
