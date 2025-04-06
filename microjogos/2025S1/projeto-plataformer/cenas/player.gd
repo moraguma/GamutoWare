c9b55c60aebb4683112a716bb003180988fe77f4
ricardo-emanuel <r200518@dac.unicamp.br>
2025-04-06 17:45:04 -0300

    Implementa código de movimentacao e condições de vitoria no player

---
+++ microjogos/2025S1/projeto-plataformer/cenas/player.gd
@@ -0,0 +1,79 @@
+extends CharacterBody2D
+
+
+var jump_speed = 250.0
+var gravity = 8.0
+var acceleration = 15.0
+var final_velocity = 75.0
+var array
+var animation_player
+var sprite
+var player
+var parent
+var timer
+var won = 0.0
+
+func _ready():
+	animation_player = get_node("AnimationPlayer")
+	sprite = get_node("Sprite2D")
+	player = get_node(".")
+	array = [get_node("../Porta"), get_node("../Porta2")]
+	parent = get_parent()
+	animation_player.play("idle")
+	timer = get_node("../Timer")
+	timer.start(6.0)
+
+
+func _process(delta):
+	for door in array:
+		if door.overlaps_body(player) and Input.is_action_just_pressed("cima"):
+			parent.register_win()
+			print("ganhei na porta")
+			get_node("../Efeito").play()
+
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity
+	
+	if animation_player.get_current_animation() == "fall" and is_on_floor():
+		animation_player.play("idle")
+	
+	if Input.is_action_pressed("direita"):
+		sprite.flip_h = false
+		animation_player.play("run")
+		if velocity.x < final_velocity:
+			velocity.x += acceleration
+	
+	elif Input.is_action_pressed("esquerda"):
+		sprite.flip_h = true
+		animation_player.play("run")
+		if velocity.x > -final_velocity:
+			velocity.x -= acceleration
+	
+	if is_on_floor() and Input.is_action_pressed("acao"):
+		velocity.y -= jump_speed
+		animation_player.play("jump")
+	
+	elif not is_on_floor() and velocity.y > 0.0:
+		animation_player.play("fall")
+	
+	if not Input.is_action_pressed("esquerda") and not Input.is_action_pressed("direita") and is_on_floor():
+		velocity.x = 0
+		animation_player.play("idle")
+	
+	move_and_slide()
+
+
+func _on_estrela_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("ganhei")
+		get_node("../Estrela").get_node("Sprite2D").visible = false
+		get_node("../Efeito").play()
+		won = 1.0
+		parent.register_win()
+
+
+func _on_timer_timeout() -> void:
+	if won == 0.0:
+		parent.register_lose()
