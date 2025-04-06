125e508a1502d42d92976d7b0465f35e70e32227
4wardAerial <4lucas.h.bertanha8@gmail.com>
2025-04-05 21:02:12 -0300

    Começa função de tirar foto

--- microjogos/2025S1/gamuto_snap/cenas/camera_player.gd
+++ microjogos/2025S1/gamuto_snap/cenas/camera_player.gd
@@ -1,11 +1,23 @@
 extends CharacterBody2D
 
-@export var speed : int = 400
+@export var speed : int = 500
+var can_snap : bool = true
 
-func get_input():
+# Pega o input do jogador para movimentação
+func get_input() -> void:
 	var input_direction : Vector2 = Input.get_vector("esquerda", "direita", "cima", "baixo")
 	velocity = input_direction * speed
 
-func _physics_process(delta):
+# Pega o input do jogador para a ação de tirar foto 
+func get_action() -> void:
+	if Input.is_action_pressed("acao"):
+		print("SNAP!")
+		can_snap = false
+
+func _process(delta) -> void:
+	if can_snap:
+		get_action()
+	
+func _physics_process(delta) -> void:
 	get_input()
 	move_and_slide()
