19040b161f8fb5b96427c736a8362095efb58035
4wardAerial <4lucas.h.bertanha8@gmail.com>
2025-04-06 16:10:30 -0300

    Adiciona efeitos quando gamuto é atingido

--- microjogos/2025S1/gamuto_snap/cenas/scripts/gamutinhos_physics.gd
+++ microjogos/2025S1/gamuto_snap/cenas/scripts/gamutinhos_physics.gd
@@ -1,15 +1,19 @@
 extends Node
 
-@onready var gamutoFisicoCena : PackedScene = preload("res://microjogos/2025S1/gamuto_snap/cenas/gamutoPhysics.tscn")
+@onready var gamutoPhysicsScene : PackedScene = preload("res://microjogos/2025S1/gamuto_snap/cenas/gamutoPhysics.tscn")
+@onready var possible_vel_x : Array = [-250, -150, -50, 50, 150, 250]
+@onready var possible_vel_ang : Array = [-50, -25, 0, 25, 50]
 
 # Função que instanceia uam cena de um Gamuto com física na cena principal
 func _on_gamutinhos_this_was_snapped(gamutoArea : Area2D) -> void:
-	var gamutoFisico = gamutoFisicoCena.instantiate()
-	pass_properties(gamutoFisico, gamutoArea)
-	add_child(gamutoFisico)
+	var gamutoPhysics = gamutoPhysicsScene.instantiate()
+	gamutoPhysics.linear_velocity.x = possible_vel_x[randi() % 6]
+	gamutoPhysics.angular_velocity = possible_vel_ang[randi() % 5]
+	pass_properties(gamutoPhysics, gamutoArea)
+	add_child(gamutoPhysics)
 
 # Função para passar os parâmetros do Gamuto que foi atingido para o Gamuto físico que deve cair da cena
-func pass_properties(gamutoFisico : RigidBody2D, gamutoArea : Area2D):
-	gamutoFisico.position = gamutoArea.global_position
-	gamutoFisico.rotation = gamutoArea.rotation
-	gamutoFisico.get_child(0).scale = gamutoArea.get_child(0).global_scale
+func pass_properties(gamutoPhysics : RigidBody2D, gamutoArea : Area2D):
+	gamutoPhysics.position = gamutoArea.global_position
+	gamutoPhysics.rotation = gamutoArea.rotation
+	gamutoPhysics.get_child(0).scale = gamutoArea.get_child(0).global_scale
--- microjogos/2025S1/gamuto_snap/cenas/scripts/gamuto_physics.gd
+++ microjogos/2025S1/gamuto_snap/cenas/scripts/gamuto_physics.gd
@@ -1,1 +1,6 @@
 extends RigidBody2D
+
+# Deleta Gamuto quando ele estiver longe o suficiente
+func _process(_delta) -> void:
+	if position.y > 1500:
+		queue_free()
