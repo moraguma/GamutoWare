63ec105f168f4d5b812a638c3fb1eb8da19e3ee0
Roaster Plant <paulo.pinho.campos@protonmail.com>
2025-04-06 19:29:51 -0300

    Bomb Zone finished

---
+++ microjogos/2025S1/Bomb_Zone/creditos.gd
@@ -0,0 +1,23 @@
+extends Resource
+
+
+# Este arquivo vai ser usado diretamente nos créditos do jogo! Dê crédito a todas as pessoaas que
+# participaram da criação do jogo e a todos os recursos externos utilizados. Lembre que a fonte que
+# será utilizada não suporte caracteres latinos como ~ ou ´
+
+
+# Título do seu jogo
+var title_pt = "Zona Bombástica"
+var title_en = "Bomb Zone"
+
+# Detalhes dos créditos do seu jogo
+var credits_pt = """
+Criado por: Paulo C. C. Pinho
+Áudio retirado de: https://opengameart.org/
+
+"""
+var credits_en = """
+Made by: Paulo C. C. Pinho
+Audio from: https://opengameart.org/
+
+"""
