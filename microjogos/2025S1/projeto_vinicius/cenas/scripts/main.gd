518dcc91388717f6caa48471cb21e13a0cc3e0a7
Vinicius Dos Santos Almeida <ra259205@donald.lab.ic.unicamp.br>
2025-04-06 18:03:29 -0300

    Jogabilidade

--- microjogos/2025S1/projeto_vinicius/cenas/scripts/main.gd
+++ microjogos/2025S1/projeto_vinicius/cenas/scripts/main.gd
@@ -26,12 +26,21 @@ func _ready():
 			NotificationCenter.notify("DO SOMETHING!")
 		Global.LANGUAGE.PT:
 			NotificationCenter.notify("FAÇA ALGO!")
+	
 
 
 # Esta função é chamada uma vez por frame e é otimizada para cálculos relacionados a física, como
 # a movimentação de um personagem. O parâmetro delta indica a quantidade de tempo que passou desde
 # a última chamada desta função. O comando pass não faz nada
 func _physics_process(delta):
+	var velocity = Vector2.UP.rotated(rotation)
+	position += velocity * delta
+	velocity = Vector2(0, 0)
+	
+	if Input.is_action_just_pressed("acao"):
+		velocity.y = -400
+	else:
+		velocity.y = 0
 	pass
 
 
@@ -70,3 +79,13 @@ func register_win():
 # Chame esta função para registrar que o jogador perdeu o jogo
 func register_lose():
 	emit_signal("lose")
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	register_win()
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	register_lose()
+	pass # Replace with function body.
