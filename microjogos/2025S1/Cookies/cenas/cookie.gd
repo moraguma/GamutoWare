2d67f9cb6c0389de1decad5e94a2ee93c0277843
Leonardo Carvalho De Luca <r288820@dac.unicamp.br>
2025-04-06 21:01:43 -0300

    Monstruosidade Cookies feito

--- microjogos/2025S1/Cookies/cenas/cookie.gd
+++ microjogos/2025S1/Cookies/cenas/cookie.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-var velocity=Vector2(0, 40)
+var velocity=Vector2(0, 200)
+var angular_velocity=10
 
 func _physics_process(delta):
 	position+=velocity*delta
+	rotation+=angular_velocity*delta
