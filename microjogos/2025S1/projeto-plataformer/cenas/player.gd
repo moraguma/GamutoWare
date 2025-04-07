8eec1bacc9453236b3ca36fbac7dfe259f23147b
Ricardo Oliveira <r200518@dac.unicamp.br>
2025-04-07 18:34:47 -0300

    Implementa efeito sonoro na animação de correr

--- microjogos/2025S1/projeto-plataformer/cenas/player.gd
+++ microjogos/2025S1/projeto-plataformer/cenas/player.gd
@@ -43,7 +43,8 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("direita"):
 		sprite.flip_h = false
-		animation_player.play("run")
+		if is_on_floor():
+			animation_player.play("run")
 		if velocity.x < 0:
 			velocity.x = 0
 		if velocity.x < final_velocity:
@@ -53,7 +54,8 @@ func _physics_process(delta):
 	
 	elif Input.is_action_pressed("esquerda"):
 		sprite.flip_h = true
-		animation_player.play("run")
+		if is_on_floor():
+			animation_player.play("run")
 		if velocity.x > 0:
 			velocity.x = 0
 		if velocity.x > -final_velocity:
@@ -62,14 +64,20 @@ func _physics_process(delta):
 			velocity.x = -final_velocity
 	
 	if is_on_floor() and Input.is_action_pressed("acao"):
+		if animation_player.current_animation == "run":
+			print("correndo")
+			animation_player.stop()
+			$AnimationPlayer/AudioStreamPlayer2D.stop()
 		velocity.y -= jump_speed
 		animation_player.play("jump")
 	
 	elif not is_on_floor() and velocity.y > 0.0:
+		$AnimationPlayer/AudioStreamPlayer2D.stop()
 		animation_player.play("fall")
 	
 	if not Input.is_action_pressed("esquerda") and not Input.is_action_pressed("direita") and is_on_floor():
 		velocity.x = 0
+		$AnimationPlayer/AudioStreamPlayer2D.stop()
 		animation_player.play("idle")
 	
 	move_and_slide()
