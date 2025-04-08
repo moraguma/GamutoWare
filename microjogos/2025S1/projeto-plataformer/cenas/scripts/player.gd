491b4dd3f99af595c17e97f194887c285040b29a
RICARDO EMANUEL DE OLIVEIRA <r200518@dac.unicamp.br@si03e03.labs.basico.unicamp.br>
2025-04-08 15:23:07 -0300

    Retira lógica que deveria estar na main

--- microjogos/2025S1/projeto-plataformer/cenas/scripts/player.gd
+++ microjogos/2025S1/projeto-plataformer/cenas/scripts/player.gd
@@ -1,41 +1,31 @@
 extends CharacterBody2D
 
 
-var jump_speed = 265.0
-var gravity = 9.0
-var acceleration = 100.0
-var final_velocity = 200.0
-var array
+var jump_speed: float = 265.0
+var gravity: float = 9.0
+var acceleration: float = 100.0
+var final_velocity: float = 200.0
 var animation_player
+var fell: bool = false
+var is_jumping = false
 var sprite
-var player
-var parent
-var timer
-var won = 0.0
-var estrela
-var fell = false
+var landing_sound
 
 func _ready():
-	animation_player = get_node("AnimationPlayer")
-	sprite = get_node("Sprite2D")
-	player = get_node(".")
-	array = [get_node("../Porta"), get_node("../Porta2")]
-	parent = get_parent()
+	animation_player = $AnimationPlayer
 	animation_player.play("idle")
-	timer = get_node("../Timer")
-	estrela = get_node("../Estrela")
-	timer.start(6.0)
+	animation_player.speed_scale = 1.0
+	sprite = $Sprite2D
+	landing_sound = $Landing_sound
 
 
 func _process(delta):
-	for door in array:
-		if door.overlaps_body(player) and Input.is_action_just_pressed("cima"):
-			parent.register_win()
-			won = 1.0
-			get_node("../Efeito_vitoria").play()
+	if animation_player.current_animation != "run":
+		animation_player.speed_scale = 1.0
 	if position.y >= 8.0 * 16 and not fell:
-		$falling.play()
-		$falling.volume_db -= 10.0
+		var falling = $Falling
+		falling.play()
+		falling.volume_db -= 10.0
 		fell = true
 
 
@@ -45,57 +35,60 @@ func _physics_process(delta):
 	
 	if animation_player.get_current_animation() == "fall" and is_on_floor():
 		animation_player.play("idle")
-		$landing_sound.play()
+		animation_player.speed_scale = 1.0
+		landing_sound.play()
+		is_jumping = false
 	
 	if Input.is_action_pressed("direita"):
 		sprite.flip_h = false
 		if is_on_floor():
 			animation_player.play("run")
 		if velocity.x < 0:
-			velocity.x = 0
-		if velocity.x < final_velocity:
-			velocity.x += acceleration * delta
+			velocity.x += 1.5 * acceleration * delta
 		else:
-			velocity.x = final_velocity
+			if velocity.x < final_velocity:
+				velocity.x += acceleration * delta
+			else:
+				velocity.x = final_velocity
 	
 	elif Input.is_action_pressed("esquerda"):
 		sprite.flip_h = true
 		if is_on_floor():
 			animation_player.play("run")
 		if velocity.x > 0:
-			velocity.x = 0
-		if velocity.x > -final_velocity:
-			velocity.x -= acceleration * delta
+			velocity.x -= 1.5 * acceleration * delta
 		else:
-			velocity.x = -final_velocity
+			if velocity.x > -final_velocity:
+				velocity.x -= acceleration * delta
+			else:
+				velocity.x = -final_velocity
 	
 	if is_on_floor() and Input.is_action_pressed("acao"):
+		is_jumping = true
 		if animation_player.current_animation == "run":
 			animation_player.stop()
 			$AnimationPlayer/AudioStreamPlayer2D.stop()
-		velocity.y -= jump_speed
+		# Essa animação não funciona se o boneco estiver em idle
+		# Em um frame após pular, o boneco ainda estava encostando no chão
+		# e isso fazia com que a animação de idle pra quando
+		# ele estava parado no chão ativasse
 		animation_player.play("jump")
+		velocity.y -= jump_speed
+		animation_player.speed_scale = 1.0
 	
 	elif not is_on_floor() and velocity.y > 0.0:
 		$AnimationPlayer/AudioStreamPlayer2D.stop()
 		animation_player.play("fall")
+		animation_player.speed_scale = 1.0
 	
-	if not Input.is_action_pressed("esquerda") and not Input.is_action_pressed("direita") and is_on_floor():
+	if not Input.is_action_pressed("esquerda") and not Input.is_action_pressed("direita") and is_on_floor() and not is_jumping:
 		velocity.x = 0
 		$AnimationPlayer/AudioStreamPlayer2D.stop()
 		animation_player.play("idle")
+		animation_player.speed_scale = 1.0
 	
+	if animation_player.current_animation == "run":
+		animation_player.speed_scale = 1.0 + abs(velocity.x) / final_velocity
+	else:
+		animation_player.speed_scale = 1.0
 	move_and_slide()
-
-
-func _on_estrela_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player") and estrela.get_node("Sprite2D").visible == true:
-		get_node("../Estrela").get_node("Sprite2D").visible = false
-		get_node("../Efeito_vitoria").play()
-		won = 1.0
-		parent.register_win()
-
-
-func _on_timer_timeout() -> void:
-	if won == 0.0:
-		parent.register_lose()
