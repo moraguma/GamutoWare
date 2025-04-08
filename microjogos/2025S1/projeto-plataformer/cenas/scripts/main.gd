ebd61de9bedd085b9a34d768f0a3d096ce72b698
RICARDO EMANUEL DE OLIVEIRA <r200518@dac.unicamp.br@si03e03.labs.basico.unicamp.br>
2025-04-08 15:23:31 -0300

    Implementa lógica que estava no Personagem e deveria estar na main e retira o Timer

--- microjogos/2025S1/projeto-plataformer/cenas/scripts/main.gd
+++ microjogos/2025S1/projeto-plataformer/cenas/scripts/main.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 # Declaração dos sinais win e lose
-
 signal win
 signal lose
+
+var won: int = 0
+var doors: Array
 
 # Estas constantes são usadas para determinar o tamanho da tela do seu jogo. Por padrão, definem uma
 # tela 1920x1080, que é padrão para monitores full HD. Caso você queira uma resolução menor para 
@@ -23,10 +25,13 @@ func _ready():
 	# ser feito para vencer o jogo. A fonte usada não suporta caracteres latinos como ~ ou ´
 	match Global.language:
 		Global.LANGUAGE.EN:
-			NotificationCenter.notify("TRY!")
+			NotificationCenter.notify("DO SOMETHING!")
 		Global.LANGUAGE.PT:
-			NotificationCenter.notify("TENTE!")
-
+			NotificationCenter.notify("FAÇA ALGO!")
+	
+	$Estrela.connect("body_entered", _on_estrela_body_entered)
+	doors = [$Porta1, $Porta2]
+	register_lose()
 
 # Esta função é chamada uma vez por frame e é otimizada para cálculos relacionados a física, como
 # a movimentação de um personagem. O parâmetro delta indica a quantidade de tempo que passou desde
@@ -39,17 +44,22 @@ func _physics_process(delta):
 # como a movimentação de um personagem. O parâmetro delta indica a quantidade de tempo que passou 
 # desde a última chamada desta função. O comando pass não faz nada
 func _process(delta):
-	pass
+	for door in doors:
+		if door.overlaps_body($Personagem) and Input.is_action_just_pressed("cima"):
+			register_win()
+			won = 1.0
+			$Efeito_vitoria.play()
 
 
 # --------------------------------------------------------------------------------------------------
 # SUAS FUNÇÕES
 # --------------------------------------------------------------------------------------------------
-
-
-# Um método genérico. Crie quantos métodos você precisar!
-func my_method():
-	pass
+func _on_estrela_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") and $Estrela/Sprite2D.visible == true:
+		$Estrela/Sprite2D.visible = false
+		$Efeito_vitoria.play()
+		won = 1.0
+		register_win()
 
 
 # --------------------------------------------------------------------------------------------------
