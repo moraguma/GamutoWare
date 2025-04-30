c4c1bdd52c69c59d4508244ab3bceb95c4ab0070
andre.terada <teradaandre@gmail.com>
2025-04-29 21:59:59 -0300

    Fix-rebase

--- microjogos/2024S1/nyan-thulu/cenas/scripts/main.gd
+++ microjogos/2024S1/nyan-thulu/cenas/scripts/main.gd
@@ -25,14 +25,7 @@ var player_is_dead = false
 
 # Esta função é chamada assim que esta cena é instanciada, ou seja, assim que seu minigame inicia
 func _ready():
-	# Verifica a linguagem do jogo e mostra texto nesta linguagem. Deve dar uma ideia do que deve
-	# ser feito para vencer o jogo. A fonte usada não suporta caracteres latinos como ~ ou ´
-	match Global.language:
-		Global.LANGUAGE.EN:
-			NotificationCenter.notify("SHOOT THE COOKIES!")
-		Global.LANGUAGE.PT:
-			NotificationCenter.notify("ATIRE NOS COOKIES!")
-			
+	
 	register_win()
 	rng.randomize()
 	spawners = [$"Spawner", $"Spawner2", $"Spawner3", $"Spawner4"]
