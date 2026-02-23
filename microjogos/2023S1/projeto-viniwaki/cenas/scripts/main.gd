e992515be346cac740160c539b0cd994dac991c0
André Yuhji Terada <teradaandre@gmail.com>
2026-02-23 04:34:24 -0300

    Fix-viniwaki-game-ref

--- microjogos/2023S1/projeto-viniwaki/cenas/scripts/main.gd
+++ microjogos/2023S1/projeto-viniwaki/cenas/scripts/main.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+var fruit_scene = preload("res://microjogos/2023S1/projeto-viniwaki/cenas/fruit.tscn")
+var slice_queues = {
+	1: [],
+	2: [],
+	3: [],
+	4: [],
+}
+
 # Declaração dos sinais win e lose
 signal win
 signal lose
@@ -18,21 +26,37 @@ const HEIGHT = 1080
 
 # Esta função é chamada assim que esta cena é instanciada, ou seja, assim que seu minigame inicia
 func _ready():
-	pass
+	register_win()
+	
 
 
 # Esta função é chamada uma vez por frame e é otimizada para cálculos relacionados a física, como
 # a movimentação de um personagem. O parâmetro delta indica a quantidade de tempo que passou desde
 # a última chamada desta função. O comando pass não faz nada
-func _physics_process(delta):
+func _physics_process(_delta):
 	pass
 
 
 # Esta função é chamada uma vez por frame e é otimizada para cálculos relacionados a renderização, 
 # como a movimentação de um personagem. O parâmetro delta indica a quantidade de tempo que passou 
 # desde a última chamada desta função. O comando pass não faz nada
-func _process(delta):
-	pass
+func _process(_delta):
+	if Input.is_action_just_pressed("cima"):
+		if slice_queues[1].size() != 0:
+			successful_cut(1)
+	
+	if Input.is_action_just_pressed("direita"):
+		if slice_queues[2].size() != 0:
+			successful_cut(2)
+	
+	if Input.is_action_just_pressed("baixo"):
+		if slice_queues[3].size() != 0:
+			successful_cut(3)
+	
+	if Input.is_action_just_pressed("esquerda"):
+		if slice_queues[4].size() != 0:
+			successful_cut(4)
+	
 
 
 # --------------------------------------------------------------------------------------------------
@@ -41,9 +65,42 @@ func _process(delta):
 
 
 # Um método genérico. Crie quantos métodos você precisar!
-func my_method():
-	pass
+func successful_cut(dir: int):
+		var f = slice_queues[dir].pop_front()
+		f.set_cut()
+		$slice.position = f.position
+		$slice.animate_once(dir)
+		$audio_slice.play()
+		$score_count.text = str(int($score_count.text) + 1)
 
+func remove_from_slice_queue(d, fruit):
+	slice_queues[d].erase(fruit)
+	$audio_lose.play()
+	register_lose()
+	
+func throw_fruit():
+	var new_fruit = fruit_scene.instantiate()
+	var dir = randi_range(1,4)
+	new_fruit.set_dir(dir)
+	
+	var fruit_spawn_loc = $Path2D/PathFollow2D
+	fruit_spawn_loc.progress_ratio = randf()
+	new_fruit.position = fruit_spawn_loc.position - Vector2(0, 1216)
+	
+	slice_queues[dir].append(new_fruit)
+	add_child(new_fruit)
+	
+	$audio_throw.play()
+	
+
+func _on_timer_timeout():
+	throw_fruit()
+
+	if randf() > 0.9:
+		throw_fruit()
+
+	if randf() > 0.5:
+		throw_fruit()
 
 # --------------------------------------------------------------------------------------------------
 # CONDIÇÕES DE VITÓRIA
