2e8a70c3f4fd3a6c867ed8e035e5667a0bf299b6
André Yuhji Terada <teradaandre@gmail.com>
2026-02-24 01:14:57 -0300

    fix-principal

--- principal/cenas/scripts/MoveScreenButton.gd
+++ principal/cenas/scripts/MoveScreenButton.gd
@@ -1,8 +1,8 @@
 extends "res://principal/cenas/scripts/PressableButton.gd"
 
 
-@export (Vector2) var movement_vector
-@export (NodePath) var next_focus_path
+@export var movement_vector : Vector2
+@export var next_focus_path : NodePath
 
 
 @onready var next_focus_node = get_node(next_focus_path)
