ad71bb4fdfb78da1d95c87a76eb6dbffd4269699
André Yuhji Terada <teradaandre@gmail.com>
2026-02-24 01:48:56 -0300

    fix-goats

--- microjogos/not_implemented/godotware_old/Gandhi_Simulator/Gandhi-simulator_original/Churchill.gd
+++ microjogos/not_implemented/godotware_old/Gandhi_Simulator/Gandhi-simulator_original/Churchill.gd
@@ -1,22 +1,22 @@
-extends CharacterBody2D
-var a = 0
-var b = 0
-var c = 0
-
-func _fixed_process(delta):
-	if c%80==0:
-		randomize()
-		c = randi()%30
-		b = c%4
-	
-	if b ==0:
-		move(Vector2(0,-6))
-	if b == 1:
-		move(Vector2(6,0))
-	if b ==2:
-		move(Vector2(0,6))
-	if b ==3:
-		move(Vector2(-6,0))
-			
-	c = c +1
-	pass
+#extends CharacterBody2D
+#var a = 0
+#var b = 0
+#var c = 0
+#
+#func _fixed_process(delta):
+	#if c%80==0:
+		#randomize()
+		#c = randi()%30
+		#b = c%4
+	#
+	#if b ==0:
+		#move(Vector2(0,-6))
+	#if b == 1:
+		#move(Vector2(6,0))
+	#if b ==2:
+		#move(Vector2(0,6))
+	#if b ==3:
+		#move(Vector2(-6,0))
+			#
+	#c = c +1
+	#pass
