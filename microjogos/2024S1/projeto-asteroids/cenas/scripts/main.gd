4cc0693ae9b6126431c20d49235e44a57d7bc84d
IsaelGabriel <isaelgsfaria@gmail.com>
2026-03-15 17:16:27 -0300

    Fix 2024S1/projeto-asteroids

--- microjogos/2024S1/projeto-asteroids/cenas/scripts/main.gd
+++ microjogos/2024S1/projeto-asteroids/cenas/scripts/main.gd
@@ -5,10 +5,6 @@ extends Node2D
 
 #= preload("res://microjogos/2024S1/projeto-asteroids/cenas/asteroid.tscn")
 
-# Declaração dos sinais win e lose
-signal win
-signal lose
-
 # Estas constantes são usadas para determinar o tamanho da tela do seu jogo. Por padrão, definem uma
 # tela 1920x1080, que é padrão para monitores full HD. Caso você queira uma resolução menor para 
 # atingir uma estética mais pixelada, você pode mudar estes números para qualquer outra resolução 
@@ -62,26 +58,6 @@ func game_start():
 	player.position = a.position
 	add_child(player)
 	pass
-
-
-# --------------------------------------------------------------------------------------------------
-# CONDIÇÕES DE VITÓRIA
-# --------------------------------------------------------------------------------------------------
-# Quando o jogo começa, ela assume que o jogador não conseguiu vencer o jogo ainda, ou seja, se não
-# acontecer nada, o jogador vai perder o jogo. A verificação se o jogador venceu o minigame é feita
-# com base na emissão dos sinais "win" e "lose". Se "win" foi o último sinal emitido, o jogador
-# vencerá o jogo, e se "lose" foi o último sinal emitido ou nenhu m sinal foi emitido, o jogador
-# perderá o jogo
-
-
-# Chame esta função para registrar que o jogador venceu o jogo
-func Minigames.register_win(self):
-	emit_signal("win")
-
-
-# Chame esta função para registrar que o jogador perdeu o jogo
-func Minigames.register_lose(self):
-	emit_signal("lose")
 
 
 func _on_start_timer_timeout():
