2e84734f6ebcb745d82d9f70416329df6db23f0e
IsaelGabriel <isaelgsfaria@gmail.com>
2026-03-15 16:36:04 -0300

    Fix 2023S1/bis-limao

--- microjogos/2023S1/bis-limao/cenas/scripts/main.gd
+++ microjogos/2023S1/bis-limao/cenas/scripts/main.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-# Declaração dos sinais win e lose
-signal win
-signal lose
-
 # Estas constantes são usadas para determinar o tamanho da tela do seu jogo. Por padrão, definem uma
 # tela 1920x1080, que é padrão para monitores full HD. Caso você queira uma resolução menor para 
 # atingir uma estética mais pixelada, você pode mudar estes números para qualquer outra resolução 
@@ -90,16 +86,10 @@ func my_method():
 # vencerá o jogo, e se "lose" foi o último sinal emitido ou nenhum sinal foi emitido, o jogador
 # perderá o jogo
 
-
-# Chame esta função para registrar que o jogador venceu o jogo
-func Minigames.register_win(self):
-	emit_signal("win")
-
-
 # Chame esta função para registrar que o jogador perdeu o jogo
 func register_lose(reason: String):
 	if alreadyLost:
 		return
-	emit_signal("lose")
+	Minigames.register_lose(self)
 	alreadyLost = true
 	get_node("Gamuto").loseAnim(reason)
