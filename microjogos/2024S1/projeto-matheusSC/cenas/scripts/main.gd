dd70af18d052f8a195e77d569d73f83e81ba246c
IsaelGabriel <isaelgsfaria@gmail.com>
2026-03-15 17:34:25 -0300

    Fix 2024S1/projeto-matheusSC

--- microjogos/2024S1/projeto-matheusSC/cenas/scripts/main.gd
+++ microjogos/2024S1/projeto-matheusSC/cenas/scripts/main.gd
@@ -13,10 +13,6 @@ var gota
 
 var crowd_anim_played = false
 
-# Declaração dos sinais win e lose
-signal win
-signal lose
-
 # Estas constantes são usadas para determinar o tamanho da tela do seu jogo. Por padrão, definem uma
 # tela 1920x1080, que é padrão para monitores full HD. Caso você queira uma resolução menor para 
 # atingir uma estética mais pixelada, você pode mudar estes números para qualquer outra resolução 
@@ -89,22 +85,3 @@ func my_method():
 func _on_timer_timeout():
 	pode_checkar = true
 	pass # Replace with function body.
-
-# --------------------------------------------------------------------------------------------------
-# CONDIÇÕES DE VITÓRIA
-# --------------------------------------------------------------------------------------------------
-# Quando o jogo começa, ela assume que o jogador não conseguiu vencer o jogo ainda, ou seja, se não
-# acontecer nada, o jogador vai perder o jogo. A verificação se o jogador venceu o minigame é feita
-# com base na emissão dos sinais "win" e "lose". Se "win" foi o último sinal emitido, o jogador
-# vencerá o jogo, e se "lose" foi o último sinal emitido ou nenhum sinal foi emitido, o jogador
-# perderá o jogo
-
-
-# Chame esta função para registrar que o jogador venceu o jogo
-func Minigames.register_win(self):
-	emit_signal("win")
-
-
-# Chame esta função para registrar que o jogador perdeu o jogo
-func Minigames.register_lose(self):
-	emit_signal("lose")
