63fd013008c03c934d8b31d8eadc54bbe8bb8971
IsaelGabriel <isaelgsfaria@gmail.com>
2026-03-15 17:11:08 -0300

    Fix 2024S1/gamufit

--- microjogos/2024S1/gamufit/cenas/scripts/main.gd
+++ microjogos/2024S1/gamufit/cenas/scripts/main.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-# Declaração dos sinais win e lose
-signal win
-signal lose
-
 # Estas constantes são usadas para determinar o tamanho da tela do seu jogo. Por padrão, definem uma
 # tela 1920x1080, que é padrão para monitores full HD. Caso você queira uma resolução menor para 
 # atingir uma estética mais pixelada, você pode mudar estes números para qualquer outra resolução 
@@ -11,19 +7,10 @@ signal lose
 const WIDTH = 1920
 const HEIGHT = 1080
 
-
-# --------------------------------------------------------------------------------------------------
-# FUNÇÕES PADRÃO
-# --------------------------------------------------------------------------------------------------
-
-# Esta função é chamada assim que esta cena é instanciada, ou seja, assim que seu minigame inicia
-func _ready():
-	pass
-
 # Chame esta função para registrar que o jogador venceu o jogo
-func Minigames.register_win(self):
-	emit_signal("win")
+func register_win():
+	Minigames.register_win(self)
 
 # Chame esta função para registrar que o jogador perdeu o jogo
-func Minigames.register_lose(self):
-	emit_signal("lose")
+func register_lose():
+	Minigames.register_lose(self)
