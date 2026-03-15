0a21e876d83efa547cdba43b75bcc407f892d6b2
IsaelGabriel <isaelgsfaria@gmail.com>
2026-03-15 17:25:34 -0300

    Fix 2024S1/projeto-FastFood

--- microjogos/2024S1/projeto-FastFood/cenas/scripts/main.gd
+++ microjogos/2024S1/projeto-FastFood/cenas/scripts/main.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-# Declaração dos sinais win e lose
-signal win
-signal lose
-
 # Estas constantes são usadas para determinar o tamanho da tela do seu jogo. Por padrão, definem uma
 # tela 1920x1080, que é padrão para monitores full HD. Caso você queira uma resolução menor para 
 # atingir uma estética mais pixelada, você pode mudar estes números para qualquer outra resolução 
@@ -62,12 +58,9 @@ func my_method():
 
 
 # Chame esta função para registrar que o jogador venceu o jogo
-func Minigames.register_win(self):
-	emit_signal("win")
+func register_win():
+	Minigames.register_win(self)
 
-# Chame esta função para registrar que o jogador perdeu o jogo
-func Minigames.register_lose(self):
-	emit_signal("lose")
 	
 
 
