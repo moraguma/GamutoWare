1d10fc380ca9a62f7f2c493103c475c709f310c3
IsaelGabriel <isaelgsfaria@gmail.com>
2026-03-15 18:02:00 -0300

    Fix missing register win functions

--- microjogos/2024S1/projeto-samuel/cenas/scripts/main.gd
+++ microjogos/2024S1/projeto-samuel/cenas/scripts/main.gd
@@ -52,27 +52,6 @@ func _process(delta):
 func my_method():
 	pass
 
-
-# --------------------------------------------------------------------------------------------------
-# CONDIÇÕES DE VITÓRIA
-# --------------------------------------------------------------------------------------------------
-# Quando o jogo começa, ela assume que o jogador não conseguiu vencer o jogo ainda, ou seja, se não
-# acontecer nada, o jogador vai perder o jogo. A verificação se o jogador venceu o minigame é feita
-# com base na emissão dos sinais "win" e "lose". Se "win" foi o último sinal emitido, o jogador
-# vencerá o jogo, e se "lose" foi o último sinal emitido ou nenhum sinal foi emitido, o jogador
-# perderá o jogo
-
-
-# Chame esta função para registrar que o jogador venceu o jogo
-func Minigames.register_win(self):
-	emit_signal("win")
-
-
-# Chame esta função para registrar que o jogador perdeu o jogo
-func Minigames.register_lose(self):
-	emit_signal("lose")
-
-
 func _on_gamuto_g_1():
 	vitoria += 1
 
