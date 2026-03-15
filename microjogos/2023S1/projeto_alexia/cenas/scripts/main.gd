1307a4fa9071fabb79ef09865fc693d67fcf35ac
IsaelGabriel <isaelgsfaria@gmail.com>
2026-03-15 16:40:26 -0300

    Fix 2023S1/projeto_alexia

--- microjogos/2023S1/projeto_alexia/cenas/scripts/main.gd
+++ microjogos/2023S1/projeto_alexia/cenas/scripts/main.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-# Declaração dos sinais win e lose
-signal win
-signal lose
-
 # Estas constantes são usadas para determinar o tamanho da tela do seu jogo. Por padrão, definem uma
 const WIDTH = 1920
 const HEIGHT = 1080
@@ -122,22 +118,3 @@ func _process(delta):
 
 # Um método genérico. Crie quantos métodos você precisar!
 
-
-# --------------------------------------------------------------------------------------------------
-# CONDIÇÕES DE VITÓRIA
-# --------------------------------------------------------------------------------------------------
-# Quando o jogo começa, ela assume que o jogador não conseguiu vencer o jogo ainda, ou seja, se não
-# acontecer nada, o jogador vai perder o jogo. A verificação se o jogador venceu o minigame é feita
-# com base na emissão dos sinais "win" e "lose". Se "win" foi o último sinal emitido, o jogador
-# vencerá o jogo, e se "lose" foi o último sinal emitido ou nenhum sinal foi emitido, o jogador
-# perderá o jogo
-
-
-# Chame esta função para registrar que o jogador venceu o jogo
-func Minigames.register_win(self):
-	emit_signal("win")
-
-
-# Chame esta função para registrar que o jogador perdeu o jogo
-func Minigames.register_lose(self):
-	emit_signal("lose")
