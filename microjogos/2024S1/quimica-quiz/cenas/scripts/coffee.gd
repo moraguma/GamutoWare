50178c5519ba8136a49bff044cf98cfcd4e9f572
André Terada <teradaandre@gmail.com>
2026-03-24 13:00:29 -0300

    add-sounds-and-credits

--- microjogos/2024S1/quimica-quiz/cenas/scripts/coffee.gd
+++ microjogos/2024S1/quimica-quiz/cenas/scripts/coffee.gd
@@ -1,20 +1,14 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_body_entered(body: Node2D) -> void: 
 	$anim.play("collect")
+	$Drink.play()
+	collision_mask = 0
 	get_parent().register_win()
 
-
 func _on_anim_animation_finished() -> void:
+	$anim.visible = false
+
+
+func _on_drink_finished() -> void:
 	queue_free()
