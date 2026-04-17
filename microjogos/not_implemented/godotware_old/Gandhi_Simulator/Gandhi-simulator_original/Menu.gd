0f830a07e985a6ffb1b7a7ccf6e8212a293071f3
André Terada <teradaandre@gmail.com>
2026-04-17 16:18:35 -0300

    comment-all-not-implemented-games-scripts

--- microjogos/not_implemented/godotware_old/Gandhi_Simulator/Gandhi-simulator_original/Menu.gd
+++ microjogos/not_implemented/godotware_old/Gandhi_Simulator/Gandhi-simulator_original/Menu.gd
@@ -1,20 +1,20 @@
-extends Node2D
-
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
-
-
-func _on_Button_pressed():
-	get_tree().change_scene_to_file("res://Instruções.tscn")
-	pass # replace with function body
-
-
-func _on_Button_2_pressed():
-	get_tree().change_scene_to_file("res://Credits.tscn")
-	pass # replace with function body
+#extends Node2D
+#
+## class member variables go here, for example:
+## var a = 2
+## var b = "textvar"
+#
+#func _ready():
+	## Called every time the node is added to the scene.
+	## Initialization here
+	#pass
+#
+#
+#func _on_Button_pressed():
+	#get_tree().change_scene_to_file("res://Instruções.tscn")
+	#pass # replace with function body
+#
+#
+#func _on_Button_2_pressed():
+	#get_tree().change_scene_to_file("res://Credits.tscn")
+	#pass # replace with function body
--- microjogos/not_implemented/godotware_old/Gandhi_Simulator/Gandhi-simulator_original/botao_credito.gd
+++ microjogos/not_implemented/godotware_old/Gandhi_Simulator/Gandhi-simulator_original/botao_credito.gd
@@ -1,15 +1,15 @@
-extends Button
-
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
-
-
-func _on_Button_pressed():
-	get_tree().change_scene_to_file("res://Menu.tscn")
-	pass # replace with function body
+#extends Button
+#
+## class member variables go here, for example:
+## var a = 2
+## var b = "textvar"
+#
+#func _ready():
+	## Called every time the node is added to the scene.
+	## Initialization here
+	#pass
+#
+#
+#func _on_Button_pressed():
+	#get_tree().change_scene_to_file("res://Menu.tscn")
+	#pass # replace with function body
