ae6604dda98e05e4753896900f45d93a229ec0e3
André Terada <teradaandre@gmail.com>
2026-04-18 11:27:18 -0300

    fix-win-lose

--- microjogos/2025S1/HImeCaking/cenas/scripts/main.gd
+++ microjogos/2025S1/HImeCaking/cenas/scripts/main.gd
@@ -1,19 +1,8 @@
 extends Node2D
 
-# Declaração dos sinais win e lose
-
-signal win
-signal lose
-
-# Estas constantes são usadas para determinar o tamanho da tela do seu jogo. Por padrão, definem uma
-# tela 1920x1080, que é padrão para monitores full HD. Caso você queira uma resolução menor para 
-# atingir uma estética mais pixelada, você pode mudar estes números para qualquer outra resolução 
-# 16:9
-const WIDTH = 1920
-const HEIGHT = 1080
-
-
 @export var arrow_nodes: Array[Sprite2D]
+
+var  bgm = "res://microjogos/2025S1/HImeCaking/recursos/sons/bgm.wav"
 
 const ARROW_QUANTITY = 3
 
@@ -98,9 +87,9 @@ func _process(delta):
 
 # Chame esta função para registrar que o jogador venceu o jogo
 func register_win():
-	emit_signal("win")
+	Minigames.register_win(self)
 
 
 # Chame esta função para registrar que o jogador perdeu o jogo
 func register_lose():
-	emit_signal("lose")
+	Minigames.register_lose(self)
