ba91451b20c756193403dcb0dd45aa8d3a11e7e7
eduardo <eduardodeoliveirascarcella@gmail.com>
2026-06-03 21:32:59 -0300

    implementando jogo

---
+++ microjogos/2026S1/projeto-eduardo/cenas/sprite_zumbi_do_ib.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+var SPEED = 180.0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.y += SPEED*delta
+	if position.y >= 650:
+		SPEED = SPEED * -1
+	pass
