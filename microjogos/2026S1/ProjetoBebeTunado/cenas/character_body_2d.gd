0a19f5a38460f39bbe336cf938d4e14296915bfd
Cakios <caio_henrique2006@outlook.com>
2026-06-13 15:42:17 -0300

    Removes unneeded files

---
+++ microjogos/2026S1/ProjetoBebeTunado/cenas/character_body_2d.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+
+const SPEED = 2000.0
+
+@onready var animacao = $"../AnimatedSprite2D"
+
+func _physics_process(_delta):
+	var direcao = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	velocity = direcao * SPEED
+
+	if direcao.x > 0 :
+		animacao.play("direita")
+	elif direcao.x < 0:
+		animacao.play("esquerda")
+	elif direcao.y > 0:
+		animacao.play("baixo")
+	elif direcao.y < 0:
+		animacao.play("cima")			
+
+
+	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body == self:
+		Minigames.register_win(self)
+	pass # Replace with function body.
