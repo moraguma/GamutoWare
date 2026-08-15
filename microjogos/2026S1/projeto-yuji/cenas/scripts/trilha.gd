9bf1c15c26da373b9fcb6f57a8029b3138e83a40
AyudaV <teradaandre@gmail.com>
2026-08-15 20:57:12 -0300

    Optimizacao jogo yuji

--- microjogos/2026S1/projeto-yuji/cenas/scripts/trilha.gd
+++ microjogos/2026S1/projeto-yuji/cenas/scripts/trilha.gd
@@ -2,7 +2,7 @@ extends StaticBody2D
 
 func _ready() -> void:
 	$CollisionShape2D.shape = SegmentShape2D.new()
-
+	$Trilha.points = [Vector2.ZERO, Vector2.ZERO]
 func setup_trail(pos:Vector2):
 	$CollisionShape2D.shape.a = pos
 	$Trilha.points[0] = pos
