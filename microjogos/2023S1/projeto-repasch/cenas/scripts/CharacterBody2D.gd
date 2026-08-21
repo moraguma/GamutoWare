28d8c98063e4ea78207076f13a191a3f51e8d202
André Terada <teradaandre@gmail.com>
2026-08-21 02:31:01 -0300

    ahhh

--- microjogos/2023S1/projeto-repasch/cenas/scripts/CharacterBody2D.gd
+++ microjogos/2023S1/projeto-repasch/cenas/scripts/CharacterBody2D.gd
@@ -2,13 +2,17 @@ extends CharacterBody2D
 
 var comi = 0
 const SPEED = 500.0
+var is_dashing = false
 func _ready() -> void:
 	$Sprite2D.frame = randi_range(0, 9)
 
 func _physics_process(delta):
 	var dir = Vector2(Input.get_action_strength("direita") - Input.get_action_strength("esquerda"), 0)
 	$Sprite2D.flip_h = dir.x >= 0.0
-	velocity = dir * SPEED
+	if Input.is_action_just_pressed("acao") and not is_dashing:
+		is_dashing = true
+		$Dash.start(0.1)
+	velocity = dir * SPEED * (1 if not is_dashing else 5)
 	move_and_slide()
 	
 func soma_comida():
@@ -25,3 +29,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		soma_comida()
 		body.queue_free()
 		$Eat.play()
+
+
+func _on_dash_timeout() -> void:
+	is_dashing = false
